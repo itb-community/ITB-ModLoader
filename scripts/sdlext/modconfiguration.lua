8bfca3f3a835c2b30f68363c2eaca956eb1d3b57
kartoFlane <tbachminski@gmail.com>
2018-04-08 01:00:35 +0200

    Reworked modconfiguration.lua to use UiBoxLayout. Formatted.

--- scripts/sdlext/modconfiguration.lua
+++ scripts/sdlext/modconfiguration.lua
@@ -1,5 +1,5 @@
 local function saveModConfig()
-	sdlext.config("modcontent.lua",function(obj)
+	sdlext.config("modcontent.lua", function(obj)
 		obj.modOptions = mod_loader:getCurrentModContent()
 		obj.modOrder = mod_loader:getCurrentModOrder()
 	end)
@@ -20,24 +20,35 @@ function configureMods()
 			return true
 		end
 
-		local frametop = Ui():width(0.6):height(0.575):pos(0.2,0.1):caption("Mod Configuration"):decorate({DecoFrame(), DecoSolid(sdl.rgb(73,92,121)), DecoFrameCaption()}):addTo(ui)
-		local scrollarea = UiScrollArea():width(1):height(1):padding(12):decorate({DecoSolid(sdl.rgb(24,28,40))})
-		frametop:add(scrollarea)
+		local frametop = Ui()
+			:width(0.6):height(0.575)
+			:pos(0.2,0.1)
+			:caption("Mod Configuration")
+			:decorate({
+				DecoFrame(),
+				DecoSolid(deco.colors.buttonbordercolor),
+				DecoFrameCaption()
+			})
+			:addTo(ui)
+
+		local scrollarea = UiScrollArea()
+			:width(1):height(1)
+			:padding(12)
+			:decorate({ DecoSolid(deco.colors.buttoncolor) })
+			:addTo(frametop)
+
+		local entryHolder = UiBoxLayout()
+			:vgap(5)
+			:width(1)
+			:addTo(scrollarea)
+
 		ui:relayout()
 		
-		local pos=0
 		local configuringMod = nil
 		
 		local function clickConfiguration(button)
 			if configuringMod then
 				local numOptions = #optionboxes[configuringMod]
-				
-				for i = configuringMod + 1, #checkboxes do
-					checkboxes[i].y = checkboxes[i].y - (numOptions * 46)
-					if configboxes[i] then
-						configboxes[i].y = configboxes[i].y - (numOptions * 46)
-					end
-				end
 				
 				for i, optionbox in pairs(optionboxes[configuringMod]) do
 					optionbox:hide()
@@ -52,12 +63,6 @@ function configureMods()
 				configuringMod = button.configi
 				local numOptions = #optionboxes[configuringMod]
 				
-				for i = configuringMod + 1, #checkboxes do
-					checkboxes[i].y = checkboxes[i].y + (numOptions * 46)
-					if configboxes[i] then
-						configboxes[i].y = configboxes[i].y + (numOptions * 46)
-					end
-				end
 				configboxes[configuringMod].decorations[2].surface = sdl.surface("resources/mods/ui/config-checked.png")
 				
 				for i, optionbox in pairs(optionboxes[configuringMod]) do
@@ -70,65 +75,116 @@ function configureMods()
 		end
 		
 		for id, option in pairs(mod_options) do
-				if mod_loader:hasMod(id) then
+			if mod_loader:hasMod(id) then
 				local mod = mod_loader.mods[id]
 				
 				if #option.options > 0 then
-					local checkbox = UiCheckbox():pospx(0, pos):heightpx(41):widthpx((scrollarea.w - scrollarea.padl - scrollarea.padr) - 43):decorate(
-						{ DecoButton(), DecoCheckbox(), DecoSurfaceOutlined(sdlext.surface(mod.icon or "resources/mods/squads/unknown.png"),nil,nil,nil,1), DecoText(mod.name) }
-					)
+					local entryBox = UiBoxLayout()
+						:vgap(0)
+						:width(1)
+						:addTo(entryHolder)
+
+					local entryHeader = UiBoxLayout()
+						:hgap(5)
+						:heightpx(41)
+						:addTo(entryBox)
+
+					local checkbox = UiCheckbox()
+						:widthpx((scrollarea.w - scrollarea.padl - scrollarea.padr) - 45)
+						:heightpx(41)
+						:decorate({
+							DecoButton(),
+							DecoCheckbox(),
+							DecoSurfaceOutlined(
+								sdlext.surface(mod.icon or "resources/mods/squads/unknown.png"),
+								nil,
+								nil,
+								nil,
+								1
+							),
+							DecoText(mod.name)
+						})
+						:addTo(entryHeader)
 					
 					checkbox.modId = id
 					checkbox.checked = modSelection[id].enabled
-					
-					scrollarea:add( checkbox )
 					table.insert(checkboxes, checkbox)
 					
-					local configbox = UiCheckbox():pospx((scrollarea.w - scrollarea.padl - scrollarea.padr) - 41, pos):heightpx(41):widthpx(41):decorate(
-						{ DecoButton(), DecoSurface(sdl.surface("resources/mods/ui/config-unchecked.png")) }
-					)
+					local configbox = UiCheckbox()
+						:widthpx(41):heightpx(41)
+						:decorate({
+							DecoButton(),
+							DecoSurface(sdl.surface("resources/mods/ui/config-unchecked.png"))
+						})
+						:addTo(entryHeader)
 					
 					configbox.configi = #checkboxes
 					configbox.onclicked = clickConfiguration
 					configboxes[configbox.configi] = configbox
-					scrollarea:add( configbox )
 					optionboxes[configbox.configi] = {}
+
+					local optionsHolder = UiBoxLayout()
+						:vgap(5)
+						:width(0.965)
+						:addTo(entryBox)
+					optionsHolder.padt = 5
+					optionsHolder.alignH = "right"
+
 					for i, opt in ipairs(option.options) do
 						local optionbox
 						
 						if opt.check then
-							optionbox = UiCheckbox():pospx(43, pos + i * 46):heightpx(41):widthpx((scrollarea.w - scrollarea.padl - scrollarea.padr) - 41):settooltip(opt.tip):decorate(
-								{ DecoButton(), DecoText(opt.name), DecoRAlign(43), DecoCheckbox() }
-							)
+							optionbox = UiCheckbox()
+								:width(1):heightpx(41)
+								:settooltip(opt.tip)
+								:decorate({
+									DecoButton(),
+									DecoText(opt.name),
+									DecoRAlign(43),
+									DecoCheckbox()
+								})
 							
 							optionbox.checked = modSelection[id].options[opt.id].enabled
 						else
 							local value = modSelection[id].options[opt.id].value
-							optionbox = UiDropDown(opt.values,opt.strings,value):pospx(43, pos + i * 46):heightpx(41):widthpx((scrollarea.w - scrollarea.padl - scrollarea.padr) - 41):settooltip(opt.tip):decorate(
-								{ DecoButton(), DecoText(opt.name), DecoDropDownText(nil,nil,nil,43), DecoDropDown() }
-							)
+							optionbox = UiDropDown(opt.values, opt.strings, value)
+								:width(1):heightpx(41)
+								:settooltip(opt.tip)
+								:decorate({
+									DecoButton(),
+									DecoText(opt.name),
+									DecoDropDownText(nil, nil, nil, 43),
+									DecoDropDown()
+								})
 						end
 						
 						optionbox.data = opt
 						
 						optionbox:hide()
-						scrollarea:add( optionbox )
-						table.insert(optionboxes[configbox.configi],optionbox)
+						optionsHolder:add( optionbox )
+						table.insert(optionboxes[configbox.configi], optionbox)
 					end
 				else	
-					local checkbox = UiCheckbox():pospx(0, pos):heightpx(41):width(1):decorate(
-						{ DecoButton(), DecoCheckbox(), DecoSurfaceOutlined(sdlext.surface(mod.icon or "resources/mods/squads/unknown.png"),nil,nil,nil,1), DecoText(mod.name) }
-					)
+					local checkbox = UiCheckbox()
+						:width(1):heightpx(41)
+						:decorate({
+							DecoButton(),
+							DecoCheckbox(),
+							DecoSurfaceOutlined(
+								sdlext.surface(mod.icon or "resources/mods/squads/unknown.png"),
+								nil,
+								nil,
+								nil,
+								1
+							),
+							DecoText(mod.name)
+						})
+						:addTo(entryHolder)
 					
 					checkbox.checked = modSelection[id].enabled
-					
 					checkbox.modId = id
-					
-					scrollarea:add( checkbox )
 					table.insert(checkboxes, checkbox)
 				end
-				
-				pos = pos + 46
 			end
 		end
 	end)
@@ -155,7 +211,8 @@ function configureMods()
 	end
 	
 	local savedOrder = {}--TODO
-	mod_loader:loadModContent(modSelection,savedOrder)
+
+	mod_loader:loadModContent(modSelection, savedOrder)
 	
 	saveModConfig()
 end
