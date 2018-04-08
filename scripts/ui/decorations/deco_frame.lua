9ae1b2dfd697681b3d09a87d847dfa52fa0ea71e
kartoFlane <tbachminski@gmail.com>
2018-04-08 18:27:54 +0200

    Modified DecoFrame to allow custom border width, not just 2

--- scripts/ui/decorations/deco_frame.lua
+++ scripts/ui/decorations/deco_frame.lua
@@ -1,38 +1,41 @@
 DecoFrame = Class.inherit(UiDeco)
-function DecoFrame:new(color, bordercolor)
+function DecoFrame:new(color, bordercolor, bordersize)
 	self.color = color or deco.colors.buttoncolor
 	self.bordercolor = bordercolor or deco.colors.buttonbordercolor
-	self.rect = sdl.rect(0,0,0,0)
+	self.bordersize = bordersize or 2
+	self.rect = sdl.rect(0, 0, 0, 0)
 end
 
 function DecoFrame:draw(screen, widget)
 	local r = widget.rect
 
 	screen:drawrect(self.color, r)
-	
+
 	self.rect.x = r.x
 	self.rect.y = r.y
-	self.rect.w = 2
+	self.rect.w = self.bordersize
 	self.rect.h = r.h
 	screen:drawrect(self.bordercolor, self.rect)
-	self.rect.x = r.x + r.w - 2
+	self.rect.x = r.x + r.w - self.bordersize
 	screen:drawrect(self.bordercolor, self.rect)
 	
 	self.rect.x = r.x
 	self.rect.y = r.y
 	self.rect.w = r.w
-	self.rect.h = 2
+	self.rect.h = self.bordersize
 	screen:drawrect(self.bordercolor, self.rect)
-	self.rect.y = r.y + r.h - 2
+	self.rect.y = r.y + r.h - self.bordersize
 	screen:drawrect(self.bordercolor, self.rect)
+
+	widget.decorationx = widget.decorationx + self.bordersize
 end
 
 function DecoFrame:apply(widget)
-	widget:padding(2)
+	widget:padding(self.bordersize)
 end
 
 function DecoFrame:unapply(widget)
-	widget:padding(-2)
+	widget:padding(-self.bordersize)
 end
 
 
@@ -56,8 +59,13 @@ function DecoFrameCaption:draw(screen, widget)
 	self.rect.h = self.height
 	
 	screen:drawrect(self.color, self.rect)
-	local offset = self.height/2 - self.surface:h()/2 + 2
-	screen:blit(self.surface, nil, r.x + offset, r.y + offset)
+	local offset = self.height/2 - self.surface:h()/2
+	screen:blit(
+		self.surface,
+		nil,
+		r.x + widget.decorationx + offset,
+		r.y + widget.decorationy + offset
+	)
 end
 
 function DecoFrameCaption:apply(widget)
