7500721d5bd0f803460b38322519f38709123be8
kartoFlane <tbachminski@gmail.com>
2018-04-18 16:48:30 +0200

    Fixed infinite console scrolling when reloading scripts and getting an error

--- scripts/sdlext/modcontent.lua
+++ scripts/sdlext/modcontent.lua
@@ -116,20 +116,28 @@ AUTO_HOOK_Mod_Content_Draw = sdl.drawHook(function(screen)
 			
 		ui:draw(screen)
 
-		if not errorFrameShown then
-			errorFrameShown = true
+		if modApi.showErrorFrame then
+			if not errorFrameShown then
+				errorFrameShown = true
+				
+				-- Schedule the error window to be shown instead of showing
+				-- it right away.
+				-- Prevents a bug where the console keeps scrolling upwards
+				-- due to the game not registering return key release event,
+				-- when using 'reload' command to reload scripts, and getting
+				-- a script error.
+				modApi:scheduleHook(20, function()
+					-- could show all errors one after another, but let's not...
+					for dir, err in pairs(mod_loader.unmountedMods) do
+						showErrorFrame(string.format("Unable to mount mod at [%s]:\n%s",dir,err))
+						break
+					end
+					mod_loader.unmountedMods = {}
 
-			if modApi.showErrorFrame then
-				-- could show all errors one after another, but let's not...
-				for dir, err in pairs(mod_loader.unmountedMods) do
-					showErrorFrame(string.format("Unable to mount mod at [%s]:\n%s",dir,err))
-					break
-				end
-				mod_loader.unmountedMods = {}
-
-				if mod_loader.firsterror then
-					showErrorFrame(mod_loader.firsterror)
-				end
+					if mod_loader.firsterror then
+						showErrorFrame(mod_loader.firsterror)
+					end
+				end)
 			end
 		end
 	elseif isInHangar then
