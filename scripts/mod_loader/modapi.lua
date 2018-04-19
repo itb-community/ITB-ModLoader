6e3859897d054ff89e1681884f677ca33fa4d49f
kartoFlane <tbachminski@gmail.com>
2018-04-19 23:58:35 +0200

    Added string operations to modApi: startsWith, endsWith, trim

--- scripts/mod_loader/modapi.lua
+++ scripts/mod_loader/modapi.lua
@@ -101,6 +101,30 @@ function modApi:elapsedTime()
 	return self.msLastElapsed
 end
 
+--[[
+	Returns true if this string starts with the prefix string
+--]]
+function modApi:stringStartsWith(str, prefix)
+	return string.sub(str,1,string.len(prefix)) == prefix
+end
+
+--[[
+	Returns true if this string ends with the suffix string
+--]]
+function modApi:stringEndsWith(str, suffix)
+	return suffix == "" or string.sub(str,-string.len(suffix)) == suffix
+end
+
+--[[
+	Trims leading and trailing whitespace from the string.
+
+	trim11 from: http://lua-users.org/wiki/StringTrim
+--]]
+function modApi:trimString(str)
+	local n = str:find"%S"
+	return n and str:match(".*%S", n) or ""
+end
+
 function modApi:splitString(test,sep)
 	if sep == nil then
 		sep = "%s"
