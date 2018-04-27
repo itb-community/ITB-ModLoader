ea0224ac9e883079d15b257bdadf643e8813b09b
kartoFlane <tbachminski@gmail.com>
2018-04-28 00:08:12 +0200

    Modloader now removes non-vanilla maps at startup. Added modApi:addMap()

--- scripts/mod_loader/modapi.lua
+++ scripts/mod_loader/modapi.lua
@@ -72,6 +72,104 @@ function modApi:init()
 		"Detritus_B",
 	}
 
+	self.defaultMaps = {
+		"acid0", "acid1", "acid2", "acid3", "acid4",
+		"acid5", "acid6", "acid7", "acid8", "acid9",
+		"acid10", "acid11", "acid12", "acid13", "acid14",
+		"acid15",
+
+		"any0", "any1", "any2", "any3", "any4",
+		"any5", "any6", "any7", "any8", "any9",
+		"any10", "any11", "any12", "any13", "any14",
+		"any15", "any16", "any17", "any18", "any19",
+		"any20", "any21", "any22", "any23", "any24",
+		"any25", "any26", "any27", "any28", "any29",
+		"any30", "any31", "any32", "any33", "any34",
+		"any35", "any36", "any37", "any38", "any39",
+		"any40", "any41", "any42", "any43", "any44",
+		"any45", "any46", "any47", "any48", "any49",
+		"any50",
+
+		"barrels", "belt",
+
+		"cave1", "cave2", "cave3", "cave4", "cave5",
+
+		"crosscrack", "crosscrack2", "crosscrack3", "crosscrack4",
+		"crosscrack5", "crosscrack6", "crosscrack7", "crosscrack8",
+		"crosscrack9", "crosscrack10", "crosscrack11", "crosscrack12",
+		"crosscrack13", "crosscrack14", "crosscrack15",
+
+		"ctf0", "ctf1", "ctf2", "ctf3", "ctf4",
+		"ctf5", "ctf6", "ctf7", "ctf8", "ctf9",
+		"ctf10", "ctf11", "ctf12", "ctf13", "ctf14",
+
+		"disposal", "disposal1", "disposal2", "disposal3", "disposal4",
+		"disposal5", "disposal6", "disposal7", "disposal8", "disposal9",
+		"disposal10", "disposal11", "disposal12", "disposal13", "disposal14",
+		"disposal15", "disposal16", "disposal17", "disposal18", "disposal19",
+		"disposal20",
+
+		"final_island", "final_mission",
+
+		"goo1", "goo2", "goo3", "goo4", "goo5", "goo6", "goo7",
+
+		"grass0", "grass1", "grass2", "grass3", "grass4",
+		"grass5", "grass6", "grass7", "grass8", "grass9",
+		"grass10",
+
+		"hightide1", "hightide2", "hightide3", "hightide4", "hightide5",
+		"hightide6", "hightide7", "hightide8", "hightide9", "hightide10",
+		"hightide11", "hightide12", "hightide13", "hightide14", "hightide15",
+		"hightide16", "hightide17", "hightide18", "hightide19", "hightide20",
+
+		"island_ending",
+
+		"matt",
+
+		"mix0", "mix1", "mix2", "mix3", "mix4", "mix5", "mix6",	"mix7", "mix8",
+		"mix9", "mix10",
+
+		"mountclear1",
+		"null",
+
+		"opendam1", "opendam2", "opendam3", "opendam4", "opendam5",
+		"opendam6", "opendam7", "opendam8", "opendam9", "opendam10",
+		"opendam11", "opendam12", "opendam13", "opendam14", "opendam15",
+		"opendam16", "opendam17", "opendam18", "opendam19", "opendam20",
+
+		"sand0", "sand1", "sand2", "sand3", "sand4",
+		"sand5", "sand6", "sand7", "sand8", "sand9",
+		"sand10", "sand11", "sand12", "sand13", "sand14",
+		"sand15",
+
+		"sand_hole1", "sand_hole2", "sand_hole3", "sand_hole4",
+
+		"snow0", "snow1", "snow2", "snow3", "snow4",
+		"snow5", "snow6", "snow7", "snow8", "snow9",
+		"snow10", "snow11", "snow12", "snow13", "snow14",
+		"snow15", "snow16", "snow17", "snow18", "snow19",
+		"snow20", "snow21", "snow22", "snow23", "snow24",
+		"snow25",
+
+		"terraformer1", "terraformer2", "terraformer3", "terraformer4",
+		"terraformer5", "terraformer6", "terraformer7", "terraformer8",
+
+		"trailer",
+
+		"train0", "train1", "train2", "train3", "train4",
+		"train5", "train6", "train7", "train8", "train9",
+		"train10", "train11", "train12", "train13", "train14",
+		"train15", "train16", "train17", "train18", "train19",
+		"train20",
+
+		"tutorial",
+
+		"volcano1", "volcano2", "volcano3", "volcano4",
+		"volcano5", "volcano6", "volcano7", "volcano8",
+		"volcano9", "volcano10"
+	}
+	self:deleteModdedMaps()
+
 	self.compareScheduledHooks = function(a, b)
 		return a.triggerTime < b.triggerTime
 	end
@@ -165,14 +263,14 @@ end
 function modApi:resetModContent()
 	self.textOverrides = {}
 	self.mod_squads = {
-		{"Rift Walkers","PunchMech", "TankMech", "ArtiMech"},
-		{"Rusting Hulks","JetMech", "RocketMech",  "PulseMech"},
-		{"Zenith Guard","LaserMech", "ChargeMech", "ScienceMech"},
-		{"Blitzkrieg","ElectricMech", "WallMech", "RockartMech"},
-		{"Steel Judoka","JudoMech", "DStrikeMech", "GravMech"},
-		{"Flame Behemoths","FlameMech", "IgniteMech", "TeleMech"},
-		{"Frozen Titans","GuardMech", "MirrorMech", "IceMech"},
-		{"Hazardous Mechs","LeapMech", "UnstableTank", "NanoMech"},
+		{ "Rift Walkers", "PunchMech", "TankMech", "ArtiMech" },
+		{ "Rusting Hulks", "JetMech", "RocketMech",  "PulseMech" },
+		{ "Zenith Guard", "LaserMech", "ChargeMech", "ScienceMech" },
+		{ "Blitzkrieg", "ElectricMech", "WallMech", "RockartMech" },
+		{ "Steel Judoka", "JudoMech", "DStrikeMech", "GravMech" },
+		{ "Flame Behemoths", "FlameMech", "IgniteMech", "TeleMech" },
+		{ "Frozen Titans", "GuardMech", "MirrorMech", "IceMech" },
+		{ "Hazardous Mechs", "LeapMech", "UnstableTank", "NanoMech" },
 	}
 	self.squad_text = {
 		"Rift Walkers",
@@ -293,6 +391,18 @@ function modApi:resetModContent()
 	local name, tbl = debug.getupvalue(oldGetPopulationTexts,1)
 	self.PopEvents = copy_table(tbl)
 	self.onGetPopEvent = {}
+end
+
+function modApi:deleteModdedMaps()
+	for i, file in pairs(os.listfiles("maps")) do
+		local mapname = string.gsub(file, "\.map", "")
+		if
+			modApi:stringEndsWith(file, ".map") and
+			not list_contains(self.defaultMaps, mapname)
+		then
+			os.remove("maps/"..file)
+		end
+	end
 end
 
 function modApi:setCurrentMod(mod)
@@ -551,6 +661,27 @@ function modApi:addOnPopEvent(fn)
 	table.insert(self.onGetPopEvent,fn)
 end
 
+function modApi:copyfile(src, dst)
+	assert(type(src) == "string")
+	assert(type(dst) == "string")
+
+	local input = io.open(src, "r")
+	assert(input, "Unable to open " .. src)
+	local content = input:read("*a")
+	input:close()
+
+	local output = io.open(dst, "w")
+	assert(output, "Unable to open " .. dst)
+	output:write(content)
+	output:close()
+end
+
+function modApi:addMap(mapfile)
+	local idx = (string.find(mapfile, "/[^/]*$") or 0) + 1
+	local mapname = string.sub(mapfile, idx)
+	self:copyfile(mapfile, "maps/"..mapname)
+end
+
 function modApi:appendAsset(resource,filePath)
 	assert(type(resource) == "string")
 	local f = io.open(filePath,"rb")
@@ -561,7 +692,7 @@ function modApi:appendAsset(resource,filePath)
 			file._meta.body = f:read("*all")
 			file._meta._fileSize = file._meta.body:len()
 			f:close()
-		
+
 			return
 		end
 	end
