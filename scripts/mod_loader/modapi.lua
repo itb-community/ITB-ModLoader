5796a09ca68c32deb5794142df1dd362e106c9d8
kartoFlane <tbachminski@gmail.com>
2018-04-30 21:38:29 +0200

    Vanilla maps are now protected from being overwritten by addMap()

--- scripts/mod_loader/modapi.lua
+++ scripts/mod_loader/modapi.lua
@@ -393,18 +393,6 @@ function modApi:resetModContent()
 	self.onGetPopEvent = {}
 end
 
-function modApi:deleteModdedMaps()
-	for i, file in pairs(os.listfiles("maps")) do
-		local mapname = string.gsub(file, "\.map", "")
-		if
-			modApi:stringEndsWith(file, ".map") and
-			not list_contains(self.defaultMaps, mapname)
-		then
-			os.remove("maps/"..file)
-		end
-	end
-end
-
 function modApi:setCurrentMod(mod)
 	self.currentMod = mod
 	self.currentModSquads[mod] = {}
@@ -676,10 +664,48 @@ function modApi:copyfile(src, dst)
 	output:close()
 end
 
-function modApi:addMap(mapfile)
-	local idx = (string.find(mapfile, "/[^/]*$") or 0) + 1
-	local mapname = string.sub(mapfile, idx)
-	self:copyfile(mapfile, "maps/"..mapname)
+function modApi:pruneExtension(filename)
+	-- gsub() returns multiple values, store the first
+	-- value in a variable so that we correctly ignore
+	-- the retvalues that come after it.
+	local r = string.gsub(filename, "\.[^\.]*$", "")
+	return r
+end
+
+--[[
+	Returns a list of names of all *.map files in maps/ directory,
+	without the .map extension
+--]]
+function modApi:getMapsList()
+	local list = {}
+
+	for i, file in pairs(os.listfiles("maps")) do
+		if modApi:stringEndsWith(file, ".map") then
+			table.insert(list, self:pruneExtension(file))
+		end
+	end
+
+	return list
+end
+
+function modApi:deleteModdedMaps()
+	for i, mapname in ipairs(self:getMapsList()) do
+		if not list_contains(self.defaultMaps, mapname) then
+			os.remove("maps/"..mapname..".map")
+		end
+	end
+end
+
+function modApi:addMap(path)
+	local idx = (string.find(path, "/[^/]*$") or 0) + 1
+	local mapfile = string.sub(path, idx)
+	local mapname = self:pruneExtension(mapfile)
+
+	if list_contains(self.defaultMaps, mapname) then
+		LOG(string.format("Unable to add map '%s', because it would overwrite a vanilla map.", path))
+	else
+		self:copyfile(path, "maps/"..mapfile)
+	end
 end
 
 function modApi:appendAsset(resource,filePath)
