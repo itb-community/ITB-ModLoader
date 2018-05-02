f343002c2e43b8a62b3bdf825ba430dc4c96ab67
kartoFlane <tbachminski@gmail.com>
2018-05-02 12:54:05 +0200

    Added modApi:loadIntoEnv

--- scripts/mod_loader/modapi.lua
+++ scripts/mod_loader/modapi.lua
@@ -471,6 +471,29 @@ function modApi:addWeapon_Texts(tbl)
 end
 
 --[[
+	Loads the specified file, loading any global variable definitions
+	into the specified table instead of the global namespace (_G).
+	The file can still access variables defined in _G, but not write to
+	them by default (unless specifically doing _G.foo = bar).
+
+	Last arg can be omitted, defaulting to an empty table.
+--]]
+function modApi:loadIntoEnv(scriptPath, envTable)
+	envTable = envTable or {}
+	assert(type(envTable) == "table", "Environment must be a table")
+	assert(type(scriptPath) == "string", "Path is not a string")
+
+	setmetatable(envTable, { __index = _G })
+	assert(pcall(setfenv(
+		assert(loadfile(scriptPath)),
+		envTable
+	)))
+	setmetatable(envTable, nil)
+
+	return envTable
+end
+
+--[[
 	Reload the settings file to have access to selected settings
 	from in-game lua scripts.
 --]]
