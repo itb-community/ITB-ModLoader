d32aae6bf19c8ba494b8955a38d650989e423752
kartoFlane <tbachminski@gmail.com>
2018-05-06 16:12:29 +0200

    Reworked error_warning to use sdlext.showTextDialog()

--- scripts/mod_loader/modui/error_warning.lua
+++ scripts/mod_loader/modui/error_warning.lua
@@ -3,35 +3,6 @@
 	if an error occurred while loading mods.
 --]]
 
-function showErrorFrame(text)
-	sdlext.uiEventLoop(function(ui, quit)
-		ui.onclicked = function(self, button)
-			quit()
-			return true
-		end
-
-		local w = 700
-		local h = 400
-		local frame = Ui()
-			:widthpx(w):heightpx(h)
-			:pospx((ui.w - w)/2, (ui.h - h)/2)
-			:caption("Error")
-			:decorate({ DecoFrame(), DecoFrameCaption() })
-			:addTo(ui)
-
-		local scroll = UiScrollArea()
-			:width(1):height(1)
-			:padding(10)
-			:decorate({ DecoSolid() })
-			:addTo(frame)
-
-		text = text or "Some mods failed to load. Check console for details."
-		UiWrappedText(text)
-			:width(1)
-			:addTo(scroll)
-	end)
-end
-
 local errorFrameShown = false
 sdlext.addMainMenuEnteredHook(function(screen, wasHangar, wasGame)
 	if modApi.showErrorFrame then
@@ -47,17 +18,20 @@ sdlext.addMainMenuEnteredHook(function(screen, wasHangar, wasGame)
 			modApi:scheduleHook(20, function()
 				-- could show all errors one after another, but let's not...
 				for dir, err in pairs(mod_loader.unmountedMods) do
-					showErrorFrame(string.format(
-						"Unable to mount mod at [%s]:\n%s",
-						dir,
-						err
-					))
+					sdlext.showTextDialog(
+						"Error",
+						string.format(
+							"Unable to mount mod at [%s]:\n%s",
+							dir,
+							err
+						)
+					)
 					break
 				end
 				mod_loader.unmountedMods = {}
 
 				if mod_loader.firsterror then
-					showErrorFrame(mod_loader.firsterror)
+					sdlext.showTextDialog("Error", mod_loader.firsterror)
 				end
 			end)
 		end
