875eb8618772cf59f046c6309b7edf71dd0e1e98
kartoFlane <tbachminski@gmail.com>
2018-05-15 20:22:38 +0200

    Version 2.2.2

--- scripts/mod_loader/modapi.lua
+++ scripts/mod_loader/modapi.lua
@@ -68,7 +68,7 @@ function modApi:init()
 		self.logger.logToFile("log.txt")
 	end
 
-	self.version = "2.2.1"
+	self.version = "2.2.2"
 	LOG("MOD-API VERSION "..self.version)
 	self.currentModSquads = {}
 	self.currentModSquadText = {}
