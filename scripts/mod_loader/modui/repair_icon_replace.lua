a4c3141c3e9566e4f3e51b9d2850322593d11181
kartoFlane <tbachminski@gmail.com>
2018-06-26 02:22:06 +0200

    Fixed repair replace icon covering up the hotkey

--- scripts/mod_loader/modui/repair_icon_replace.lua
+++ scripts/mod_loader/modui/repair_icon_replace.lua
@@ -23,6 +23,9 @@ sdlext.addUiRootCreatedHook(function(screen, uiRoot)
 	iconHolder.translucent = true
 	iconHolder.visible = false
 
+	iconHolder.clipRect1 = sdl.rect(0, 0, 32, 65)
+	iconHolder.clipRect2 = sdl.rect(0, 0, 18, 15)
+
  	iconHolder.draw = function(self, screen)
  		self.visible = false
  		if Pawn and repairIcon:wasDrawn() then
@@ -30,12 +33,21 @@ sdlext.addUiRootCreatedHook(function(screen, uiRoot)
 			if srf then
 	 			self.x = repairIcon.x
 	 			self.y = repairIcon.y
+	 			self.clipRect1.x = self.x
+	 			self.clipRect1.y = self.y
+	 			self.clipRect2.x = self.x
+	 			self.clipRect2.y = self.y + self.clipRect1.h
 	 			self.visible = true
 	 			self.decorations[1].surface = srf
 			end
  		end
 
+ 		screen:clip(self.clipRect1)
  		Ui.draw(self, screen)
+ 		screen:unclip()
+ 		screen:clip(self.clipRect2)
+ 		Ui.draw(self, screen)
+ 		screen:unclip()
  	end
 end)
 
