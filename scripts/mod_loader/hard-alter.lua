25541d7b5505d73cd67c713cf17d00e0ca52255f
kartoFlane <tbachminski@gmail.com>
2018-09-09 11:09:28 +0200

    Reworked GlobalizeGlobaTexts function to be reusable for other files/variables

--- scripts/mod_loader/hard-alter.lua
+++ scripts/mod_loader/hard-alter.lua
@@ -4,27 +4,38 @@
 --]]
 
 --[[
-	Rewrites text.lua so that Global_Texts table inside
+	Rewrites the specified game file so that the local variable inside
 	of it becomes globally accessible.
 --]]
-local function GlobalizeGlobalTexts()
-	if not Global_Texts then
-		local path = "scripts/text.lua"
-		local file = io.open(path, "rb")
-
+local function globalizeLocalVariable(filePath, variableName)
+	if not _G[variableName] then
+		local file = assert(io.open(filePath, "r"), "Failed to open file: "..filePath)
 		local content = file:read("*all")
 		file:close()
 		file = nil
 
-		local index = string.find(content, "local Global_Texts") + 6
+		local prefix = "local "
+		local index = string.find(content, prefix..variableName)
 
-		content = string.sub(content, index)
-		file = io.open(path, "w+b")
+		if not index or index == -1 then
+			error(string.format(
+				"Could not find local variable '%s' in file '%s'.",
+				variableName,
+				filePath
+			))
+		end
+
+		content, index = string.gsub(content, prefix..variableName, variableName)
+
+		file = assert(io.open(filePath, "w"), "Failed to open file: "..filePath)
 		file:write(content)
 		file:close()
+		file = nil
 
-		dofile(path)
-		LOG("Globalized Global_Texts")
+		dofile(filePath)
+
+		LOG("Globalized", variableName)
 	end
 end
-GlobalizeGlobalTexts()
+
+globalizeLocalVariable("scripts/text.lua", "Global_Texts")
