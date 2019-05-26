5c8a8201dd77bc78e2e1298b73fed405480daf68
kartoFlane <tbachminski@gmail.com>
2019-05-26 16:17:46 +0200

    Update package.bat to automatically pack release directory into a zip

--- package.bat
+++ package.bat
@@ -3,6 +3,7 @@
 IF EXIST release RMDIR /q /s release
 MKDIR release
 
+REM Copy required files into release directory...
 COPY /V lua5.1.dll release\lua5.1.dll
 COPY /V lua5.1-original.dll release\lua5.1-original.dll
 COPY /V opengl32.dll release\opengl32.dll
@@ -12,3 +13,6 @@ COPY /V uninstall.bat release\uninstall.bat
 XCOPY mods release\mods /s /e /i
 XCOPY scripts release\scripts /s /e /i
 XCOPY resources release\resources /s /e /i
+
+REM ...and pack it into a zip
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('release', 'ITB-ModLoader-#.#.#.zip'); }"
