1a66351f6add9ff29de194262ebdc81fd1667571
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2019-06-24 00:58:08 +0200

    Initial suggestion for Board hooks/override functions.
    Adds hooks and overrides for all the vanilla Board functions.
    example use:
    `modApi:addBoardSpaceDamageHook(function(spaceDamage)
        -- hook into
        -- or override by returning new parameters:
        return SpaceDamage(Point(0,0), 1)
    end`
    A negative consequence of this, is that calling the functions without any parameters in the console will no longer display valid signatures.

--- scripts/mod_loader/modapi/board.lua
+++ scripts/mod_loader/modapi/board.lua
@@ -1,6 +1,144 @@
 local DequeList = require("scripts/mod_loader/deque_list")
+local boardOverrides = {}
+local doInit = true
+local hooks = {}
+
+local boardKeys = {
+	"AddAlert",
+	"AddAnimation",
+	"AddBurst", -- 2 signatures
+	"AddEffect",
+	"AddPawn",
+	"AddTeleport",
+	"AddUniqueBuilding",
+	"BlockSpawn",
+	"Bounce",
+	"ClearBlockSpawns",
+	"ClearSpace",
+	"Crack",
+	"DamageSpace", -- 2 signatures
+	"Fade",
+	"GetAnotherPlayerPawn",
+	"GetBuildingCount",
+	"GetBuildings",
+	"GetBusyState",
+	"GetDistance",
+	"GetDistanceToBuilding",
+	"GetDistanceToPawn",
+	"GetEnemyCount",
+	"GetMechDamage",
+	"GetPath",
+	"GetPawn", -- 2 signatures
+	"GetPawnCount",
+	"GetPawnSpace",
+	"GetPawnTeam", -- 2 signatures
+	"GetPawns",
+	"GetRandomBuilding",
+	"GetReachable",
+	"GetSimplePath",
+	"GetSimpleReachable",
+	"GetSize",
+	"GetSpawnCount", -- 2 signatures
+	"GetTerrain", -- 2 signatures
+	"GetTurn",
+	"GetZone",
+	"IsAcid",
+	"IsBlocked",
+	"IsBuilding", -- 2 signatures
+	"IsDamaged",
+	"IsDangerous",
+	"IsDangerousItem",
+	"IsEdge",
+	"IsEnvironmentDanger",
+	"IsFire",
+	"IsFrozen",
+	"IsItem",
+	"IsPawnAlive",
+	"IsPawnSpace",
+	"IsPawnTeam",
+	"IsPod",
+	"IsPowered",
+	"IsSafe",
+	"IsSmoke",
+	"IsSpawning",
+	"IsTargeted",
+	"IsTerrain",
+	"IsUniqueBuilding",
+	"IsValid", -- 2 signatures
+	"IsVines",
+	"IsWall",
+	"LockBomb",
+	"MarkFlashing",
+	"MarkSpaceColor",
+	"MarkSpaceDamage",
+	"MarkSpaceDesc",
+	"MarkSpaceImage",
+	"MarkSpaceSimpleColor",
+	"Ping",
+	"RandomizeTerrain",
+	"RemovePawn", -- 2 signatures
+	"RemoveShield",
+	"SetAcid",
+	"SetCustomTile",
+	"SetDangerous",
+	"SetFrozen",
+	"SetItem",
+	"SetLava",
+	"SetPopulated",
+	"SetSmoke",
+	"SetTerrain",
+	"SetTerrainIcon",
+	"SetWall",
+	"SetWeather",
+	"Slide",
+	"SpawnPawn", -- 2 signatures
+	"SpawnQueued",
+	"StartMechTravel",
+	"StartPopEvent",
+	"StartShake",
+	"StopWeather",
+}
 
 BoardClass = Board
+
+local oldSetBoard = SetBoard
+function SetBoard(board)
+	if board and doInit then
+		for i, fn in pairs(boardOverrides) do
+			local oldFunc = board[i]
+			
+			BoardClass[i] = function(self, ...)
+				return oldFunc(self, fn(self, ...))
+			end
+		end
+		
+		doInit = nil
+	end
+	
+	oldSetBoard(board)
+end
+
+for _, key in ipairs(boardKeys) do
+	modApi:AddHook("board".. key)
+	
+	modApi["fireBoard".. key .."Hooks"] = function(...)
+		local result = {...}
+		
+		for _, fn in ipairs(modApi["board".. key .."Hooks"]) do
+			local output = {fn(unpack(result))}
+			
+			if output and #output > 0 then
+				result = output
+			end
+		end
+		
+		return unpack(result)
+	end
+	
+	boardOverrides[key] = function(self, ...)
+		return modApi["fireBoard".. key .."Hooks"](...)
+	end
+end
 
 BoardClass.MovePawnsFromTile = function(self, loc)
 	Tests.AssertEquals("userdata", type(self), "Argument #0")
