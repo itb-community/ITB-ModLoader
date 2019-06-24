59d96cd9dc206ebfd87803403b54e5330f2c1221
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2019-06-24 22:42:57 +0200

    Added Board.IsTipImage and fixed Board.IsMissionBoard
    Board is unavailable in Mission.Initialize.
    The soonest we see the Board is in Mission.BaseStart.
    
    isMission must be set in BaseUpdate because it gets reset each time we look at a tipimage, and probably other times as well.
    
    If Board was made 'sticky' somehow, we could get away with setting isMission in BaseStart and GetEnvironment instead. The latter is called before BaseUpdate after loading a game, and Board is available.

--- scripts/mod_loader/altered/missions.lua
+++ scripts/mod_loader/altered/missions.lua
@@ -56,6 +56,7 @@ end
 
 local oldBaseUpdate = Mission.BaseUpdate
 function Mission:BaseUpdate()
+	Board.isMission = true
 	modApi.current_mission = self
 	modApi:processRunLaterQueue(self)
 
@@ -196,6 +197,7 @@ function Mission:BaseStart(suppressHooks)
 		end
 	end
 
+	Board.isMission = true
 	self.Board = Board
 	self.QueuedSpawns = {}
 
@@ -280,6 +282,7 @@ end
 -- ////////////////////////////////////////////////////////////////////
 
 function Mission_Test:BaseStart()
+	Board.isMission = true
 	Mission.BaseStart(self, true)
 
 	for i, hook in ipairs(modApi.testMechEnteredHooks) do
