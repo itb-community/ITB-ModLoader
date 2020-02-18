1ca103615a1c977ce7a0fd9e08a2898140018c6f
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-02-18 19:10:32 +0100

    Added code to read all profiles
    Added function `modApi:getProfileList()`

--- scripts/mod_loader/modapi/statistics.lua
+++ scripts/mod_loader/modapi/statistics.lua
@@ -7,14 +7,31 @@ Placeholder_Personality = CreatePilotPersonality("NULL", "placeholder")
 
 local ProfileDataAffirmed = false
 
-function modApi:affirmProfileData()
-	local profile = modApi:loadProfile()
+function modApi:getProfileList()
 	
-	-- TODO: do for all profiles
+	local result = {}
+	local path_savedata = GetSavedataLocation()
+	local directory = io.popen(string.format("dir %q /B /AD", path_savedata))
 	
-	if profile == nil then
-		return
+	for dir_profile in directory:lines() do
+		if dir_profile:match("^profile_") then
+			
+			local path_profile = string.format("%s%s/profile.lua", path_savedata, dir_profile)
+			
+			if self:fileExists(path_profile) then
+				local profile = self:loadIntoEnv(path_profile).Profile
+				
+				table.insert(result, profile)
+			end
+		end
 	end
+	
+	directory:close()
+	
+	return result
+end
+
+function modApi:affirmProfileData()
 	
 	if ProfileDataAffirmed then
 		return
@@ -38,54 +55,58 @@ function modApi:affirmProfileData()
 		_G[id] = _G[id] or placeholder
 	end
 	
-	local i = 0
-	local stat_tracker = profile.stat_tracker
-	local score = stat_tracker["current"]
-	
-	if not score then
-		score = stat_tracker["score".. i]
-		i = i + 1
-	end
-	
-	while score do
-		for _, mech in ipairs(score.mechs) do
-			affirmUnit(mech, Placeholder_Mech)
+	local profile_list = self:getProfileList()
+	for _, profile in ipairs(profile_list) do
+		
+		local i = 0
+		local stat_tracker = profile.stat_tracker
+		local score = stat_tracker["current"]
+		
+		if not score then
+			score = stat_tracker["score".. i]
+			i = i + 1
 		end
 		
-		for _, weapon in ipairs(score.weapons) do
-			affirmUnit(weapon, Placeholder_Weapon)
-		end
-		
-		local j = 0
-		while true do
-			local pilot = score["pilot".. j]
-			
-			if pilot == nil then
-				break
+		while score do
+			for _, mech in ipairs(score.mechs) do
+				affirmUnit(mech, Placeholder_Mech)
 			end
 			
-			affirmUnit(pilot.id, Placeholder_Pilot)
-			affirmPersonality(pilot.id, Placeholder_Personality)
+			for _, weapon in ipairs(score.weapons) do
+				affirmUnit(weapon, Placeholder_Weapon)
+			end
 			
-			j = j + 1
+			local j = 0
+			while true do
+				local pilot = score["pilot".. j]
+				
+				if pilot == nil then
+					break
+				end
+				
+				affirmUnit(pilot.id, Placeholder_Pilot)
+				affirmPersonality(pilot.id, Placeholder_Personality)
+				
+				j = j + 1
+			end
+			
+			score = stat_tracker["score".. i]
+			
+			i = i + 1
 		end
 		
-		score = stat_tracker["score".. i]
+		for _, pilot in ipairs(profile.pilots) do
+			affirmUnit(pilot, Placeholder_Pilot)
+			affirmPersonality(pilot, Placeholder_Personality)
+		end
 		
-		i = i + 1
-	end
-	
-	for _, pilot in ipairs(profile.pilots) do
-		affirmUnit(pilot, Placeholder_Pilot)
-		affirmPersonality(pilot, Placeholder_Personality)
-	end
-	
-	for pilot, _ in ipairs(stat_tracker.pilots) do
-		affirmUnit(pilot, Placeholder_Pilot)
-		affirmPersonality(pilot, Placeholder_Personality)
-	end
-	
-	for enemy, _ in ipairs(stat_tracker.enemies) do
-		affirmUnit(enemy, Placeholder_Enemy)
+		for pilot, _ in ipairs(stat_tracker.pilots) do
+			affirmUnit(pilot, Placeholder_Pilot)
+			affirmPersonality(pilot, Placeholder_Personality)
+		end
+		
+		for enemy, _ in ipairs(stat_tracker.enemies) do
+			affirmUnit(enemy, Placeholder_Enemy)
+		end
 	end
 end
