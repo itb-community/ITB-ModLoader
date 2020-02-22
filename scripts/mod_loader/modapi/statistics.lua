2acc10b5e8131c451055997201e666e88281904f
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-02-22 13:34:41 +0100

    Removes Pilots and Statistics errors. (#27)
    * Removes Pilots and Statistics errors.
    
    Added initialization of all entries of pilots/mechs/weapons/enemies found in the profile data, in order to be able to remove a mod without having to suffer lingering errors on startup and statistics screen.
    
    * Moved function to separate file and added placeholder graphics
    
    The game wants to check for pilots before we even get to the main menu.
    Changed the creation of placeholders to account for it.
    TODO: do for all profiles, as the game checks pilots for them all.
    
    * Added placeholder files
    
    * Added code to read all profiles
    
    Added function `modApi:getProfileList()`
    
    * Revert "Removes Pilots and Statistics errors."
    
    This reverts commit 14f7ea37c449d0fa79c1296e2063df18ae0e90b9.
    
    * Removed border
    
    Border is automatically added by the game.
    
    * Fixed mistakes

---
+++ scripts/mod_loader/modapi/statistics.lua
@@ -0,0 +1,112 @@
+
+Placeholder_Mech = PunchMech:new{Image = "placeholder_mech"}
+Placeholder_Pilot = Pilot:new{}
+Placeholder_Weapon = Skill:new{Icon = "weapons/placeholder_weapon.png"}
+Placeholder_Enemy = Hornet1:new{Name = "Missing Mod", Image = "placeholder_enemy"}
+Placeholder_Personality = CreatePilotPersonality("NULL", "placeholder")
+
+local ProfileDataAffirmed = false
+
+function modApi:getProfileList()
+	
+	local result = {}
+	local path_savedata = GetSavedataLocation()
+	local directory = io.popen(string.format("dir %q /B /AD", path_savedata))
+	
+	for dir_profile in directory:lines() do
+		if dir_profile:match("^profile_") then
+			
+			local path_profile = string.format("%s%s/profile.lua", path_savedata, dir_profile)
+			
+			if self:fileExists(path_profile) then
+				local profile = self:loadIntoEnv(path_profile).Profile
+				
+				table.insert(result, profile)
+			end
+		end
+	end
+	
+	directory:close()
+	
+	return result
+end
+
+function modApi:affirmProfileData()
+	
+	if ProfileDataAffirmed then
+		return
+	end
+	
+	ProfileDataAffirmed = true
+	
+	local function affirmPersonality(id)
+		if id == "" then
+			return
+		end
+		
+		Personality[id] = Personality[id] or Placeholder_Personality
+	end
+	
+	local function affirmUnit(id, placeholder)
+		if id == "" then
+			return
+		end
+		
+		_G[id] = _G[id] or placeholder:new()
+	end
+	
+	local profile_list = self:getProfileList()
+	for _, profile in ipairs(profile_list) do
+		
+		local i = 0
+		local stat_tracker = profile.stat_tracker
+		local score = stat_tracker["current"]
+		
+		if not score then
+			score = stat_tracker["score".. i]
+			i = i + 1
+		end
+		
+		while score do
+			for _, mech in ipairs(score.mechs) do
+				affirmUnit(mech, Placeholder_Mech)
+			end
+			
+			for _, weapon in ipairs(score.weapons) do
+				affirmUnit(weapon, Placeholder_Weapon)
+			end
+			
+			local j = 0
+			while true do
+				local pilot = score["pilot".. j]
+				
+				if pilot == nil then
+					break
+				end
+				
+				affirmUnit(pilot.id, Placeholder_Pilot)
+				affirmPersonality(pilot.id, Placeholder_Personality)
+				
+				j = j + 1
+			end
+			
+			score = stat_tracker["score".. i]
+			
+			i = i + 1
+		end
+		
+		for _, pilot in ipairs(profile.pilots) do
+			affirmUnit(pilot, Placeholder_Pilot)
+			affirmPersonality(pilot, Placeholder_Personality)
+		end
+		
+		for pilot, _ in pairs(stat_tracker.pilots) do
+			affirmUnit(pilot, Placeholder_Pilot)
+			affirmPersonality(pilot, Placeholder_Personality)
+		end
+		
+		for enemy, _ in pairs(stat_tracker.enemies) do
+			affirmUnit(enemy, Placeholder_Enemy)
+		end
+	end
+end
