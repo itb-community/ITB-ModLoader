1489ed7a1396c29c609ba941736db30d4f163742
kartoFlane <tbachminski@gmail.com>
2020-03-16 18:01:33 +0100

    Add try() to more easily handle functions that can throw an error

--- scripts/mod_loader/modapi/global.lua
+++ scripts/mod_loader/modapi/global.lua
@@ -225,3 +225,39 @@ function screenPointToTile(sourcePointScreenSpace, findTileEdge)
 
 	return nil
 end
+
+--[[
+	A function construct to emulate try-catch-finally blocks from regular programming languages.
+
+	Usage:
+		try(function()
+			-- some code that throw a lua error
+			error("error!")
+		end)
+		:catch(function(err)
+			-- function to handle the error
+			LOG(err)
+		end)
+		:finally(function()
+			-- actions to always perform, even when the function in `try` doesn't
+			-- throw an error
+			LOG("Finally block")
+		end)
+--]]
+function try(func)
+	local ok, err = pcall(func)
+	return {
+		catch = function(self, handle)
+			if not ok and handle then
+				handle(err)
+			end
+			return self
+		end,
+		finally = function(self, handle)
+			if handle then
+				handle()
+			end
+			return self
+		end
+	}
+end
