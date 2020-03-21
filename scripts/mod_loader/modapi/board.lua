4bbadb461d5c4ece8630980d429c5f0ae90af265
kartoFlane <tbachminski@gmail.com>
2020-03-21 17:25:14 +0100

    Fix Board:IsDamaged not working correctly with damaged tiles changed into forests

--- scripts/mod_loader/modapi/board.lua
+++ scripts/mod_loader/modapi/board.lua
@@ -656,6 +656,22 @@ function InitializeBoardClass(board)
 		
 		return buildings
 	end
+
+	-- if a damaged tile is changed into a forest, the original IsDamaged function
+	-- will return incorrectly 'true'
+	local oldIsDamaged = board.IsDamaged
+	BoardClass.IsDamaged = function(self, loc)
+		local isDamaged = oldIsDamaged(self, loc)
+		-- this will return false for forest fires - this is fine, since
+		-- in that case the tile is actually damaged.
+		local isForest = self:IsTerrain(loc, TERRAIN_FOREST)
+
+		if isDamaged then
+			return not isForest
+		else
+			return false
+		end
+	end
 	
 	-- TODO: add function SetAcid(board, bool, bool) with instant acid application/removal
 end
