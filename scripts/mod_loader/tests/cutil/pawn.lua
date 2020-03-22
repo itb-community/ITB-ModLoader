c0a8c8eae4e2652882153c3c8dc5f8b4add501bd
kartoFlane <tbachminski@gmail.com>
2020-03-22 15:49:21 +0100

    Added tests for GetWeaponType, SetOwner, GetImpactMaterial, SetImpactMaterial

--- scripts/mod_loader/tests/cutil/pawn.lua
+++ scripts/mod_loader/tests/cutil/pawn.lua
@@ -119,6 +119,27 @@ testsuite.test_WeaponCount_ShouldCountWeapons_WhenTwoWeapons = buildPawnTest({
 	end
 })
 
+testsuite.test_GetWeaponType_ShouldReturnCorrectWeapons = buildPawnTest({
+	prepare = function()
+		pawnId = Board:SpawnPawn("RocketMech")
+		pawn = Board:GetPawn(pawnId)
+
+		expectedWeapon1 = RocketMech.SkillList[1]
+		expectedWeapon2 = RocketMech.SkillList[2]
+	end,
+	execute = function()
+		actualWeapon1 = pawn:GetWeaponType(1)
+		actualWeapon2 = pawn:GetWeaponType(2)
+	end,
+	check = function()
+		assertEquals(expectedWeapon1, actualWeapon1, "GetWeaponType(1) returned incorrect weapons")
+		assertEquals(expectedWeapon2, actualWeapon2, "GetWeaponType(2) returned incorrect weapons")
+	end,
+	cleanup = function()
+		Board:RemovePawn(pawn)
+	end
+})
+
 testsuite.test_SpawnedMinions_ShouldHaveOwnerSetToPawnThatCreatedThem = buildPawnTest({
 	prepare = function()
 		caster = PAWN_FACTORY:CreatePawn("Spider1")
@@ -148,9 +169,79 @@ testsuite.test_SpawnedMinions_ShouldHaveOwnerSetToPawnThatCreatedThem = buildPaw
 	end,
 	cleanup = function()
 		Board:RemovePawn(caster)
-		Board:RemovePawn(target)
 		Board:SetTerrain(casterLoc, casterTerrain)
+		if target then
+			Board:RemovePawn(target)
+		end
 		Board:SetTerrain(targetLoc, targetTerrain)
+	end
+})
+
+testsuite.test_SetOwner_ShouldChangeOwner = buildPawnTest({
+	prepare = function()
+		ownerId = Board:SpawnPawn("PunchMech")
+		owner = Board:GetPawn(ownerId)
+
+		minionId = Board:SpawnPawn("PunchMech")
+		minion = Board:GetPawn(minionId)
+	end,
+	execute = function()
+		minion:SetOwner(ownerId)
+	end,
+	check = function()
+		local actualOwnerId = minion:GetOwner()
+		assertEquals(ownerId, actualOwnerId, "SetOwner() did not change pawn owner")
+	end,
+	cleanup = function()
+		Board:RemovePawn(owner)
+		Board:RemovePawn(minion)
+	end
+})
+
+testsuite.test_GetImpactMaterial_ShouldReturnCorrectImpactMaterial = buildPawnTest({
+	prepare = function()
+		mechPawnId = Board:AddPawn("PunchMech")
+		mechPawn = Board:GetPawn(mechPawnId)
+
+		vekPawnId = Board:AddPawn("Scorpion1")
+		vekPawn = Board:GetPawn(vekPawnId)
+
+		expectedMechImpactMaterial = PunchMech.ImpactMaterial
+		expectedVekImpactMaterial = Scorpion1.ImpactMaterial
+	end,
+	execute = function()
+		actualMechImpactMaterial = mechPawn:GetImpactMaterial()
+		actualVekImpactMaterial = vekPawn:GetImpactMaterial()
+	end,
+	check = function()
+		assertEquals(expectedMechImpactMaterial, actualMechImpactMaterial, "GetImpactMaterial() returned incorrect impact material")
+		assertEquals(expectedVekImpactMaterial, actualVekImpactMaterial, "GetImpactMaterial() returned incorrect impact material")
+	end,
+	cleanup = function()
+		Board:RemovePawn(mechPawn)
+		Board:RemovePawn(vekPawn)
+	end
+})
+
+testsuite.test_SetImpactMaterial_ShouldChangeImpactMaterial = buildPawnTest({
+	prepare = function()
+		pawnId = Board:AddPawn("PunchMech")
+		pawn = Board:GetPawn(pawnId)
+
+		expectedImpactMaterial = IMPACT_INSECT
+		originalImpactMaterial = PunchMech.ImpactMaterial
+	end,
+	execute = function()
+		pawn:SetImpactMaterial(expectedImpactMaterial)
+	end,
+	check = function()
+		local actualImpactMaterial = pawn:GetImpactMaterial()
+
+		assertEquals(expectedImpactMaterial, actualImpactMaterial, "SetImpactMaterial() did not change impact material")
+		assertEquals(originalImpactMaterial, PunchMech.ImpactMaterial, "SetImpactMaterial() changed impact material on the pawn table")
+	end,
+	cleanup = function()
+		Board:RemovePawn(pawn)
 	end
 })
 
