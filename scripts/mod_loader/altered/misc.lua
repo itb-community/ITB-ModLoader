1c875563f1a80ca20caadb06dda28e78b4945f18
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-03-23 12:59:07 +0100

    Globalize FinalEnemyList

--- scripts/mod_loader/altered/misc.lua
+++ scripts/mod_loader/altered/misc.lua
@@ -207,6 +207,23 @@ function initializeDecks()
 	PilotList = oldPilotList
 end
 
+FinalEnemyList = FinalEnemyList or {
+	"Firefly", "Hornet", "Scarab", "Scorpion",
+	"Crab", "Beetle", "Digger", "Blobber",
+	"Jelly_Lava", "Jelly_Lava", "Jelly_Lava" --make it more likely
+}
+
+local oldGetSpawnList = GameObject.GetSpawnList
+function GameObject:GetSpawnList(island)
+	local result = oldGetSpawnList(self, island)
+	
+	if island == 5 then -- final island!
+		result = FinalEnemyList
+	end
+	
+	return result
+end
+
 Effect.GetLuaString = function(self)
 	return string.format("CreateEffect(%s)", save_table(self.data))
 end
