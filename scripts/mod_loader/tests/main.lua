abea9c4c17e97cf45a81226970e753b0e396f0e8
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-04-12 15:27:34 +0200

    Added code to run individual tests

--- scripts/mod_loader/tests/main.lua
+++ scripts/mod_loader/tests/main.lua
@@ -17,3 +17,22 @@ testsuite.cutil = require(rootpath.."cutil/main")
 --]]
 
 Testsuites = testsuite
+
+function RunIndividualTest(testFn)
+  local resultTable = {}
+
+  local ok, result = pcall(function() return testFn(resultTable) end)
+  resultTable.ok = resultTable.ok or ok
+  resultTable.result = resultTable.result or result
+
+  modApi:conditionalHook(
+    function() return not ok or not resultTable.ok or resultTable.result ~= nil or resultTable.done end,
+    function()
+      if resultTable.ok and resultTable.result == true then
+        LOG("SUCCESS")
+      else
+        LOG("FAILURE:", resultTable.result)
+      end
+    end
+  )
+end
