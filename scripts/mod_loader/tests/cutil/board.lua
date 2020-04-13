1923f9117f678e2db6e553cc715980f9a9c69b6b
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-04-13 17:17:26 +0200

    Added test for Board.SetFire

--- scripts/mod_loader/tests/cutil/board.lua
+++ scripts/mod_loader/tests/cutil/board.lua
@@ -70,4 +70,48 @@ testsuite.test_SetFrozen_ShouldFreezePawnsAndMountains = buildPawnTest({
 	end
 })
 
+testsuite.test_SetFire_ShouldSetFireToTerrainAndPawns = buildPawnTest({
+	-- pawn and tile should light on fire and be extinguished.
+	prepare = function()
+		pawn = Board:GetPawn(Board:AddPawn("PunchMech"))
+		
+		pawnLoc = pawn:GetSpace()
+		tileLoc = getRandomLocation()
+		
+		defaultTerrain = Board:GetTerrain(tileLoc)
+		Board:SetTerrain(tileLoc, TERRAIN_ROAD)
+		
+		local fx = SkillEffect()
+		fx.iFire = EFFECT_REMOVE
+		
+		fx.loc = pawnLoc; Board:AddEffect(fx)
+		fx.loc = tileLoc; Board:AddEffect(fx)
+	end,
+	execute = function()
+		-- Light pawn and tile on fire.
+		Board:SetFire(pawnLoc)
+		Board:SetFire(tileLoc)
+		
+		actualPawnFireState = Board:IsFire(pawnLoc)
+		actualTileFireState = Board:IsFire(tileLoc)
+		
+		-- Extinguish pawn and tile.
+		Board:SetFire(pawnLoc, false)
+		Board:SetFire(tileLoc, false)
+		
+		actualPawnExtinguishedState = Board:IsFire(pawnLoc)
+		actualTileExtinguishedState = Board:IsFire(tileLoc)
+	end,
+	check = function()
+		assertEquals(true, actualPawnFireState, "Pawn was incorrectly not on fire")
+		assertEquals(true, actualTileFireState, "Tile was incorrectly not on fire")
+		assertEquals(false, actualPawnExtinguishedState, "Pawn was incorrectly not extinguished")
+		assertEquals(false, actualTileExtinguishedState, "Tile was incorrectly not extinguished")
+	end,
+	cleanup = function()
+		Board:RemovePawn(pawn)
+		Board:SetTerrain(tileLoc, defaultTerrain)
+	end
+})
+
 return testsuite
