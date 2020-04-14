c7b77a5e396e37ad566c933da5e28ce07b1afe08
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-04-14 17:13:12 +0200

    Updated test for Board.SetTerrain to use Board.IsDamaged in place of the now removed Board.IsDamagedVanilla

--- scripts/mod_loader/tests/cutil/board.lua
+++ scripts/mod_loader/tests/cutil/board.lua
@@ -467,12 +467,12 @@ testsuite.test_SetTerrain_ShouldAlterTerrainProperly = buildPawnTest({
 		Board:SetTerrain(loc, TERRAIN_MOUNTAIN_CRACKED)
 		
 		actualCrackedMountainTerrain = Board:GetTerrain(loc)
-		actualCrackedMountainDamagedState = Board:IsDamagedVanilla(loc)
+		actualCrackedMountainDamagedState = Board:IsDamaged(loc)
 		
 		Board:SetTerrain(loc, TERRAIN_ICE_CRACKED)
 		
 		actualCrackedIceTerrain = Board:GetTerrain(loc)
-		actualCrackedIceDamagedState = Board:IsDamagedVanilla(loc)
+		actualCrackedIceDamagedState = Board:IsDamaged(loc)
 	end,
 	check = function()
 		assertEquals(expectedDestroyedMountainTerrain, actualDestroyedMountainTerrain, "Destroyed mountain terrain was incorrect")
