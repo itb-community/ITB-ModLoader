ca45045ed91f4686743a93e0616a86236b111bb4
kartoFlane <tbachminski@gmail.com>
2020-05-08 21:23:04 +0200

    Move text manipulation logic to a dedicated class

--- scripts/mod_loader/ui/widgets/textbox.lua
+++ scripts/mod_loader/ui/widgets/textbox.lua
@@ -2,15 +2,13 @@ UiTextbox = Class.inherit(Ui)
 
 function UiTextbox:new()
 	Ui.new(self)
-	self.textBuffer = ""
+	self.textBuffer = EditableTextBuffer()
 	self.caretPosition = 0
 	self.selectionMarker = nil
 
-	self.onTextChanged = Event()
-	self.onCaretPositionChanged = Event()
-	self.onSelectionChanged = Event()
-
-	self:setupEventHandlers()
+	self.onTextChanged = self.textBuffer.onTextChanged
+	self.onCaretPositionChanged = self.textBuffer.onCaretPositionChanged
+	self.onSelectionChanged = self.textBuffer.onSelectionChanged
 end
 
 function UiTextbox:keydown(keycode)
@@ -50,29 +48,19 @@ end
 
 function UiTextbox:handleArrows(keycode)
 	if sdlext.isShiftDown() then
-		self.selectionMarker = self.selectionMarker or self.caretPosition
-
-		local oldMarker = self.selectionMarker
 		if keycode == SDLKeycodes.ARROW_RIGHT then
-			self.selectionMarker = math.min(string.len(self.textBuffer), self.selectionMarker + 1)
-			self.onSelectionChanged:fire(oldMarker, self.selectionMarker)
+			self.textBuffer:moveSelectionMarkerNextCharacter()
 			return true
 		elseif keycode == SDLKeycodes.ARROW_LEFT then
-			self.selectionMarker = math.max(0, self.selectionMarker - 1)
-			self.onSelectionChanged:fire(oldMarker, self.selectionMarker)
+			self.textBuffer:moveSelectionMarkerPreviousCharacter()
 			return true
 		end
 	else
-		self.selectionMarker = nil
-
-		local oldPos = self.caretPosition
 		if keycode == SDLKeycodes.ARROW_RIGHT then
-			self.caretPosition = math.min(string.len(self.textBuffer), self.caretPosition + 1)
-			self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+			self.textBuffer:moveCaretNextCharacter()
 			return true
 		elseif keycode == SDLKeycodes.ARROW_LEFT then
-			self.caretPosition = math.max(0, self.caretPosition - 1)
-			self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+			self.textBuffer:moveCaretPreviousCharacter()
 			return true
 		end
 	end
@@ -81,82 +69,31 @@ function UiTextbox:handleArrows(keycode)
 end
 
 function UiTextbox:handlePrintableCharacter(keycode)
-	local oldText = self.textBuffer
-	local oldPos = self.caretPosition
-
-	self.textBuffer = string.insert(self.textBuffer, self.caretPosition, keycode)
-	self.caretPosition = self.caretPosition + 1
-
-	self.onTextChanged:fire(oldText, self.textBuffer)
-	self.onCaretPositionChanged(oldPos, self.caretPosition)
+	self.textBuffer:insertCharacter(keycode)
 end
 
-function UiTextbox:hasSelection()
-	return self.selectionMarker ~= nil
-end
-
-function UiTextbox:handleRemoveSelection()
-	if not self:hasSelection() then
+function UiTextbox:handleDeleteSelection()
+	if not self.textBuffer:hasSelection() then
 		return
 	end
 
-	local oldText = self.textBuffer
-	local oldPos = self.caretPosition
-
-	local selectionStart = math.min(self.caretPosition, self.selectionMarker)
-	local selectionEnd = math.max(self.caretPosition, self.selectionMarker)
-
-	-- TODO: check for off-by-one errors
-	local leading = self.textBuffer:sub(0,  selectionStart)
-	local trailing = self.textBuffer:sub(selectionEnd)
-
-	self.textBuffer = leading .. trailing
-	self.caretPosition = selectionStart
-	self.selectionMarker = nil
-
-	self.onTextChanged:fire(oldText, self.textBuffer)
-	self.onCaretPositionChanged(oldPos, self.caretPosition)
+	self.textBuffer:deleteSelection()
 end
 
 function UiTextbox:handleBackspace()
-	if self:hasSelection() then
-		self:handleRemoveSelection()
+	if self.textBuffer:hasSelection() then
+		self:handleDeleteSelection()
 		return
 	end
 
-	if self.caretPosition == 0 then
-		return
-	end
-
-	local oldText = self.textBuffer
-	local oldPos = self.caretPosition
-
-	local leading = self.textBuffer:sub(0, self.caretPosition - 1)
-	local trailing = self.textBuffer:sub(self.caretPosition + 1)
-
-	self.textBuffer = leading .. trailing
-	self.caretPosition = math.max(0, self.caretPosition - 1)
-
-	self.onTextChanged:fire(oldText, self.textBuffer)
-	self.onCaretPositionChanged(oldPos, self.caretPosition)
+	self.textBuffer:deletePreviousCharacter()
 end
 
 function UiTextbox:handleDelete()
-	if self:hasSelection() then
-		self:handleRemoveSelection()
+	if self.textBuffer:hasSelection() then
+		self:handleDeleteSelection()
 		return
 	end
 
-	if self.caretPosition == #self.textBuffer then
-		return
-	end
-
-	local oldText = self.textBuffer
-
-	local leading = self.textBuffer:sub( 0, self.caretPosition)
-	local trailing = self.textBuffer:sub(self.caretPosition + 2)
-
-	self.textBuffer = leading .. trailing
-
-	self.onTextChanged:fire(oldText, self.textBuffer)
+	self.textBuffer:deleteNextCharacter()
 end
---
+++ scripts/mod_loader/ui/widgets/textbuffer.lua
@@ -0,0 +1,112 @@
+EditableTextBuffer = Class.new()
+
+function EditableTextBuffer:new(text)
+	self.text = text or ""
+	self.caretPosition = 0
+	self.selectionMarker = nil
+
+	self.onTextChanged = Event()
+	self.onCaretPositionChanged = Event()
+	self.onSelectionChanged = Event()
+end
+
+function EditableTextBuffer:insertCharacter(keycode)
+	local oldText = self.text
+	local oldPos = self.caretPosition
+
+	self.text = string.insert(self.text, self.caretPosition, keycode)
+	self.caretPosition = self.caretPosition + 1
+
+	self.onTextChanged:fire(oldText, self.text)
+	self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+end
+
+function EditableTextBuffer:moveCaretPreviousCharacter()
+	local oldPos = self.caretPosition
+	self.caretPosition = math.max(0, self.caretPosition - 1)
+	self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+end
+
+function EditableTextBuffer:moveCaretNextCharacter()
+	local oldPos = self.caretPosition
+	self.caretPosition = math.min(string.len(self.text), self.caretPosition + 1)
+	self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+end
+
+function EditableTextBuffer:moveSelectionMarkerPreviousCharacter()
+	self.selectionMarker = self.selectionMarker or self.caretPosition
+	local oldMarker = self.selectionMarker
+
+	self.selectionMarker = math.max(0, self.selectionMarker - 1)
+	self.onSelectionChanged:fire(oldMarker, self.selectionMarker)
+end
+
+function EditableTextBuffer:moveSelectionMarkerNextCharacter()
+	self.selectionMarker = self.selectionMarker or self.caretPosition
+	local oldMarker = self.selectionMarker
+
+	self.selectionMarker = math.min(string.len(self.text), self.selectionMarker + 1)
+	self.onSelectionChanged:fire(oldMarker, self.selectionMarker)
+end
+
+function EditableTextBuffer:hasSelection()
+	return self.selectionMarker ~= nil
+end
+
+function EditableTextBuffer:deselect()
+	self.selectionMarker = nil
+end
+
+function EditableTextBuffer:deleteSelection()
+	local oldText = self.text
+	local oldPos = self.caretPosition
+
+	local selectionStart = math.min(self.caretPosition, self.selectionMarker)
+	local selectionEnd = math.max(self.caretPosition, self.selectionMarker)
+
+	-- TODO: check for off-by-one errors
+	local leading = self.text:sub(0,  selectionStart)
+	local trailing = self.text:sub(selectionEnd)
+
+	self.text = leading .. trailing
+	self.caretPosition = selectionStart
+	self.selectionMarker = nil
+
+	self.onTextChanged:fire(oldText, self.text)
+	self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+end
+
+function EditableTextBuffer:deletePreviousCharacter()
+	if self.caretPosition == 0 then
+		return
+	end
+
+	local oldText = self.text
+	local oldPos = self.caretPosition
+
+	local leading = self.text:sub(0, self.caretPosition - 1)
+	local trailing = self.text:sub(self.caretPosition + 1)
+
+	self.text = leading .. trailing
+	self.caretPosition = math.max(0, self.caretPosition - 1)
+
+	self.onTextChanged:fire(oldText, self.text)
+	self.onCaretPositionChanged:fire(oldPos, self.caretPosition)
+end
+
+function EditableTextBuffer:deleteNextCharacter()
+	if self.caretPosition == #self.text then
+		return
+	end
+
+	local oldText = self.text
+
+	local leading = self.text:sub( 0, self.caretPosition)
+	local trailing = self.text:sub(self.caretPosition + 2)
+
+	self.text = leading .. trailing
+
+	self.onTextChanged:fire(oldText, self.text)
+end
+
+
