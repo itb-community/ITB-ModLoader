0af8706756ebb264cfcb33f0085b50b987f292a3
kartoFlane <tbachminski@gmail.com>
2020-09-29 00:19:27 +0200

    Version 2.5.4

--- scripts/mod_loader/modapi/init.lua
+++ scripts/mod_loader/modapi/init.lua
@@ -7,7 +7,7 @@ function modApi:init()
 	Settings = self:loadSettings()
 	ApplyModLoaderConfig(LoadModLoaderConfig())
 
-	self.version = "2.5.3"
+	self.version = "2.5.4"
 	LOG("MOD-API VERSION "..self.version)
 
 	if not self:fileExists("resources/resource.dat.bak") then
