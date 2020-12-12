b883cae9f07d99f36f8098ccaeecaa3af0661dad
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2020-12-12 18:06:50 +0100

    Added DecoSurfaceAligned, a general ui decoration for aligned surfaces

--- scripts/mod_loader/ui/decorations/deco_surface.lua
+++ scripts/mod_loader/ui/decorations/deco_surface.lua
@@ -47,3 +47,37 @@ function DecoSurfaceOutlined:draw(screen, widget)
 
 	DecoSurface.draw(self, screen, widget)
 end
+
+DecoSurfaceAligned = Class.inherit(DecoSurface)
+function DecoSurfaceAligned:new(surface, alignH, alignV)
+	DecoSurface.new(self, surface)
+	self.alignH = alignH
+	self.alignV = alignV
+end
+
+function DecoSurfaceAligned:draw(screen, widget)
+	if self.surface == nil then return end
+	local r = widget.rect
+	local x, y
+	
+	if self.alignH == nil or self.alignH == "left" then
+		x = r.x + widget.decorationx
+		widget.decorationx = widget.decorationx + self.surface:w()
+	elseif self.alignH == "center" then
+		x = math.floor(r.x + widget.decorationx + r.w / 2 - self.surface:w() / 2)
+		widget.decorationx = widget.decorationx + self.surface:w()
+	elseif self.alignH == "right" then
+		x = r.x - widget.decorationx + r.w - self.surface:w()
+		widget.decorationx = widget.decorationx - self.surface:w()
+	end
+	
+	if self.alignV == nil or self.alignV == "top" then
+		y = r.y + widget.decorationy
+	elseif self.alignV == "center" then
+		y = math.floor(r.y + widget.decorationy + r.h / 2 - self.surface:h() / 2)
+	elseif self.alignV == "bottom" then
+		y = r.y + widget.decorationy + r.h - self.surface:h()
+	end
+	
+	screen:blit(self.surface, nil, x, y)
+end
