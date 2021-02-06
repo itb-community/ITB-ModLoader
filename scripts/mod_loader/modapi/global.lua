4a1fe01cf5a2403ab85259252acbfc859cb6f4ab
kartoFlane <tbachminski@gmail.com>
2021-02-06 22:48:31 +0100

    Added bootstrap scripts
    These are scripts that are intended to set up mod loader modules that are more-or-less independent, and that the rest of the mod loader depends on in one way or another

--- scripts/mod_loader/modapi/global.lua
+++ scripts/mod_loader/modapi/global.lua
@@ -56,36 +56,6 @@ function GetCurrentMission()
 	return modApi.current_mission
 end
 
-function list_indexof(list, value)
-	for k, v in ipairs(list) do
-		if value == v then
-			return k
-		end
-	end
-
-	return -1
-end
-
--- Returns true if tables are equal, false otherwise
-function compare_tables(tbl1, tbl2)
-	local r = true
-	for k, v in pairs(tbl1) do
-		if type(v) == "table" then
-			if not compare_tables(tbl1[k], tbl2[k]) then
-				return false
-			end
-		elseif type(v) == "userdata" then
-			-- can't compare userdata, ignore
-		else
-			if tbl1[k] ~= tbl2[k] then
-				return false
-			end
-		end
-	end
-
-	return true
-end
-
 Emitter_Blank = Emitter:new({
     timer = 0,
     lifespan = 0,
@@ -224,47 +194,6 @@ function screenPointToTile(sourcePointScreenSpace, findTileEdge)
 	end
 
 	return nil
-end
-
---[[
-	A function construct to emulate try-catch-finally blocks from regular programming languages.
-
-	Usage:
-		try(function()
-			-- some code that throw a lua error
-			error("error!")
-		end)
-		:catch(function(err)
-			-- function to handle the error
-			LOG(err)
-		end)
-		:finally(function()
-			-- actions to always perform, even when the function in `try` doesn't
-			-- throw an error
-			LOG("Finally block")
-		end)
---]]
-function try(func)
-	local ok, err = pcall(func)
-	local handled = ok
-	return {
-		catch = function(self, handle)
-			if not ok then
-				if handle then
-					handle(err)
-					handled = true
-				end
-			end
-			return self
-		end,
-		finally = function(self, handle)
-			handle()
-			if not handled then
-				error(err)
-			end
-			return self
-		end
-	}
 end
 
 function isUserdataPoint(var)
