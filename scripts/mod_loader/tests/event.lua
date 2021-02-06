a87cd2d1157ce41dcd99edc6fc79d64af9ce0267
kartoFlane <tbachminski@gmail.com>
2021-02-06 23:40:47 +0100

    Added test for shortcircuited events

--- scripts/mod_loader/tests/event.lua
+++ scripts/mod_loader/tests/event.lua
@@ -1,11 +1,6 @@
 local testsuite = Tests.Testsuite()
 testsuite.name = "Events system tests"
 
-local assertTrue = Assert.True
-local assertFalse = Assert.False
-local assertEquals = Assert.Equals
-local assertNotEquals = Assert.NotEquals
-
 function testsuite.test_EventFire()
 	local event = Event()
 
@@ -17,9 +12,29 @@ function testsuite.test_EventFire()
 	local expectedValue = 5
 	event:fire(expectedValue)
 
-	assertNotEquals(0, value, "Event:fire() did not notify the subscriber")
-	assertEquals(expectedValue, value, "Event:fire() did not pass arguments to the subscriber")
-	assertFalse(sub:isClosed(), "Subscriber was marked as closed, even though it did not unsubscribe.")
+	Assert.NotEquals(0, value, "Event:fire() did not notify the subscriber")
+	Assert.Equals(expectedValue, value, "Event:fire() did not pass arguments to the subscriber")
+	Assert.False(sub:isClosed(), "Subscriber was marked as closed, even though it did not unsubscribe.")
+
+	return true
+end
+
+function testsuite.test_EventNotifyAllSubscribers()
+	local event = Event()
+
+	local notified1 = false
+	local notified2 = false
+	event:subscribe(function()
+		notified1 = true
+	end)
+	event:subscribe(function()
+		notified2 = true
+	end)
+
+	event:fire()
+
+	Assert.True(notified1, "Event:fire() did not notify the first subscriber")
+	Assert.True(notified2, "Event:fire() did not notify the second subscriber")
 
 	return true
 end
@@ -35,10 +50,10 @@ function testsuite.test_EventUnsubscribe()
 	local unsubResult = sub:unsubscribe()
 	event:fire()
 
-	assertTrue(unsubResult, "Event:unsubscribe() returned false for valid subscriber")
-	assertTrue(sub:isClosed(), "Subscriber was not marked as closed after it unsubscribed.")
-	assertFalse(event:isSubscribed(sub), "Event:isSubscribed() did not return false for its subscriber after it unsubscribed.")
-	assertFalse(fired, "Subscriber got notified after it unsubscribed from the event.")
+	Assert.True(unsubResult, "Event:unsubscribe() returned false for valid subscriber")
+	Assert.True(sub:isClosed(), "Subscriber was not marked as closed after it unsubscribed.")
+	Assert.False(event:isSubscribed(sub), "Event:isSubscribed() did not return false for its subscriber after it unsubscribed.")
+	Assert.False(fired, "Subscriber got notified after it unsubscribed from the event.")
 
 	return true
 end
@@ -55,8 +70,8 @@ function testsuite.test_EventUnsubscribeWithFunction()
 	local unsubResult = event:unsubscribe(fn)
 	event:fire()
 
-	assertTrue(unsubResult, "Event:unsubscribe() returned false for valid subscriber")
-	assertFalse(fired, "Subscriber got notified after it unsubscribed from the event.")
+	Assert.True(unsubResult, "Event:unsubscribe() returned false for valid subscriber")
+	Assert.False(fired, "Subscriber got notified after it unsubscribed from the event.")
 
 	return true
 end
@@ -70,7 +85,7 @@ function testsuite.test_EventUnsubscribeAll()
 
 	event:unsubscribeAll()
 
-	assertEquals(0, #event.subscribers, "Event:unsubscribeAll() did not remove all subscribers from the event.")
+	Assert.Equals(0, #event.subscribers, "Event:unsubscribeAll() did not remove all subscribers from the event.")
 
 	return true
 end
@@ -87,8 +102,8 @@ function testsuite.test_EventTeardown()
 
 	sub:unsubscribe()
 
-	assertTrue(fired, "Subscriber's teardown function was not invoked when it unsubscribed.")
-	assertTrue(sub:isClosed(), "Subscriber was not marked as closed after it unsubscribed.")
+	Assert.True(fired, "Subscriber's teardown function was not invoked when it unsubscribed.")
+	Assert.True(sub:isClosed(), "Subscriber was not marked as closed after it unsubscribed.")
 
 	return true
 end
@@ -99,8 +114,8 @@ function testsuite.test_EventIsSubscribed()
 
 	local subA = eventA:subscribe(function() end)
 
-	assertTrue(eventA:isSubscribed(subA), "Event:isSubscribed() did not return true for its own subscriber.")
-	assertFalse(eventB:isSubscribed(subA), "Event:isSubscribed() did not return false for foreign subscriber.")
+	Assert.True(eventA:isSubscribed(subA), "Event:isSubscribed() did not return true for its own subscriber.")
+	Assert.False(eventB:isSubscribed(subA), "Event:isSubscribed() did not return false for foreign subscriber.")
 
 	return true
 end
@@ -111,7 +126,33 @@ function testsuite.test_EventIsSubscribedWithFunction()
 	local fn = function() end
 	event:subscribe(fn)
 
-	assertTrue(event:isSubscribed(fn), "Event:isSubscribed() did not return true for its own subscriber.")
+	Assert.True(event:isSubscribed(fn), "Event:isSubscribed() did not return true for its own subscriber.")
+
+	return true
+end
+
+function testsuite.test_EventOptionShortcircuit()
+	local event = Event({ [Event.SHORTCIRCUIT] = true })
+
+	local notified1 = false
+	local notified2 = false
+	local notified3 = false
+	event:subscribe(function()
+		notified1 = true
+	end)
+	event:subscribe(function()
+		notified2 = true
+		return true
+	end)
+	event:subscribe(function()
+		notified3 = true
+	end)
+
+	event:fire()
+
+	Assert.True(notified1, "Event:fire() did not notify the first subscriber")
+	Assert.True(notified2, "Event:fire() did not notify the second subscriber")
+	Assert.False(notified3, "Event:fire() notified the third subscriber, after shortcircuit")
 
 	return true
 end
