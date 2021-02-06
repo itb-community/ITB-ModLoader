267cee90851afca8a00e6435bf254d34bb4e2eb5
kartoFlane <tbachminski@gmail.com>
2021-02-06 23:22:07 +0100

    Implement Event options

--- scripts/mod_loader/bootstrap/event.lua
+++ scripts/mod_loader/bootstrap/event.lua
@@ -13,18 +13,14 @@ function Subscription:unsubscribe()
 	return self.event:unsubscribe(self)
 end
 
---[[
-	Adds a teardown function to this subscription, which will be executed when
-	this subscription is unsubscribed from its event.
---]]
+--- Adds a teardown function to this subscription, which will be executed when
+--- this subscription is unsubscribed from its event.
 function Subscription:addTeardown(fn)
 	assert(type(fn) == "function", "Subscription.addTeardown: first argument must be a function\n"..debug.traceback())
 	table.insert(self.teardownFns, fn)
 end
 
---[[
-	Fires teardown functions, and removes them once completed.
---]]
+--- Fires teardown functions, and removes them once completed.
 function Subscription:executeTeardown()
 	for _, teardownFn in ipairs(self.teardownFns) do
 		teardownFn()
@@ -32,41 +28,47 @@ function Subscription:executeTeardown()
 	self.teardownFns = nil
 end
 
---[[
-	Returns true if this subscription is closed - ie. no longer valid and no longer notified of events.
---]]
+--- Returns true if this subscription is closed - ie. no longer valid and no longer notified of events.
 function Subscription:isClosed()
 	return self.event == nil
 end
 
------------------------------------------
---[[
-	Event class that allows to easily create events - or 'hooks', as we tend to call them.
-
-	Usage:
-		local myEvent = Event()
-		local subscription = myEvent:subscribe(function(someArg1, someArg2)
-			LOG("myEvent has been fired with args: ", someArg1, someArg2)
-		end)
-
-		-- Once you're no longer interested in receiving notifications from this event,
-		-- you can unsubscribe using the `subscription` object:
-		subscription:unsubscribe()
-
-		-- Unlike hooks, events are not cleaned up by default when loading, so if you define
-		-- your own event, you have to take care of removing subscriptions when it makes sense
-		-- in your case:
-		myEvent:unsubscribeAll()
---]]
+--- Event class that allows to easily create events - or 'hooks', as we tend to call them.
+---
+--- Usage:
+--- 	local myEvent = Event()
+--- 	local subscription = myEvent:subscribe(function(someArg1, someArg2)
+--- 		LOG("myEvent has been fired with args: ", someArg1, someArg2)
+--- 	end)
+---
+--- 	-- Once you're no longer interested in receiving notifications from this event,
+--- 	-- you can unsubscribe using the `subscription` object:
+--- 	subscription:unsubscribe()
+---
+--- 	-- Unlike hooks, events are not cleaned up by default when loading, so if you define
+--- 	-- your own event, you have to take care of removing subscriptions when it makes sense
+--- 	-- in your case:
+--- 	myEvent:unsubscribeAll()
 Event = Class.new()
 
-function Event:new()
+--- When this event option is true, the event allows its subscribers to shortcircuit
+--- processing by returning 'true', preventing subscribers that come after from being notified
+--- of that particular event dispatch.
+Event.SHORTCIRCUIT = "shortcircuit"
+
+function Event:new(options)
+	assert(options == nil or type(options) == "table", "Event.new: first argument must be a table\n"..debug.traceback())
 	self.subscribers = {}
+	self.options = {}
+
+	if options then
+		if options[Event.SHORTCIRCUIT] then
+			self.options[Event.SHORTCIRCUIT] = true
+		end
+	end
 end
 
---[[
-	Subscribes a function to this event; this call is analogous to modApi:add__Hook() in old hooks API.
---]]
+--- Subscribes a function to this event; this call is analogous to modApi:add__Hook() in old hooks API.
 function Event:subscribe(fn)
 	assert(type(fn) == "function", "Event.subscribe: first argument must be a function\n"..debug.traceback())
 	local sub = Subscription(self)
@@ -77,9 +79,7 @@ function Event:subscribe(fn)
 	return sub
 end
 
---[[
-	Returns true if the specified object is subscribed to this Event. False otherwise.
---]]
+--- Returns true if the specified object is subscribed to this Event. False otherwise.
 function Event:isSubscribed(subscription)
 	if type(subscription) == "table" and Class.instanceOf(subscription, Subscription) then
 		if subscription:isClosed() then
@@ -98,11 +98,9 @@ function Event:isSubscribed(subscription)
 	return false
 end
 
---[[
-	Cancels the specified subscription, making it invalid and no longer notified
-	of the event being fired.
-	Returns true if successfully unsubscribed, false otherwise.
---]]
+--- Cancels the specified subscription, making it invalid and no longer notified
+--- of the event being fired.
+--- Returns true if successfully unsubscribed, false otherwise.
 function Event:unsubscribe(subscription)
 	local argType = type(subscription)
 	assert(argType == "table" or argType == "function", "Event.unsubscribe: first argument must be a table or a function\n"..debug.traceback())
@@ -139,12 +137,10 @@ function Event:unsubscribe(subscription)
 	return true
 end
 
---[[
-	Removes all existing subscriptions from this event, preventing them from being
-	notified unless registered again.
-	Generally, only the code that 'owns' the event should call this function, as
-	part of cleanup.
---]]
+--- Removes all existing subscriptions from this event, preventing them from being
+--- notified unless registered again.
+--- Generally, only the code that 'owns' the event should call this function, as
+--- part of cleanup.
 function Event:unsubscribeAll()
 	local snapshot = shallow_copy(self.subscribers)
 	for _, sub in ipairs(snapshot) do
@@ -159,25 +155,29 @@ end
 local function pack2(...) return {n=select('#', ...), ...} end
 local function unpack2(t) return unpack(t, 1, t.n) end
 
---[[
-	Fires this event, notifying all subscribers and passing all arguments
-	that have been passed to this function to them.
-	Arguments are passed as-is without any cloning or protection, so if you
-	pass a table, and one subscriber modifies it, the changes will propagate
-	to subsequent subscribers.
---]]
+--- Fires this event, notifying all subscribers and passing all arguments
+---	that have been passed to this function to them.
+---	Arguments are passed as-is without any cloning or protection, so if you
+---	pass a table, and one subscriber modifies it, the changes will propagate
+---	to subsequent subscribers.
 function Event:fire(...)
 	local args = pack2(...)
 	local snapshot = shallow_copy(self.subscribers)
 	for _, sub in ipairs(snapshot) do
-		local ok, err = pcall(function() sub.fn(unpack2(args)) end)
+		local ok, errorOrResult = pcall(function() return sub.fn(unpack2(args)) end)
 
-		if not ok then
-			if isStackOverflowError(err) then
-				error(err)
+		if not ok and errorOrResult then
+			if isStackOverflowError(errorOrResult) then
+				error(errorOrResult)
 			else
-				LOG("An event callback failed: ", err)
+				LOG("An event callback failed: ", errorOrResult)
+			end
+		elseif ok then
+			if errorOrResult and self.options[Event.SHORTCIRCUIT] then
+				return true
 			end
 		end
 	end
+
+	return false
 end
