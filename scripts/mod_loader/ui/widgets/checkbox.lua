6b17233bd18ee9191e725a196e2df58cee699be1
kartoFlane <tbachminski@gmail.com>
2021-02-06 23:47:51 +0100

    Rename Event.fire to Event.dispatch

--- scripts/mod_loader/ui/widgets/checkbox.lua
+++ scripts/mod_loader/ui/widgets/checkbox.lua
@@ -9,7 +9,7 @@ end
 function UiCheckbox:clicked(button)
 	if button == 1 then
 		self.checked = not self.checked
-		self.onToggled:fire(self.checked)
+		self.onToggled:dispatch(self.checked)
 	end
 	
 	return Ui.clicked(self, button)
