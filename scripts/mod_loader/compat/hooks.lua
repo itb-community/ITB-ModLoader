f93dd1390bd87b107654765db584e3b0dc268dc0
kartoFlane <tbachminski@gmail.com>
2021-02-07 00:16:11 +0100

    Add compat for hook-removing functions

--- scripts/mod_loader/compat/hooks.lua
+++ scripts/mod_loader/compat/hooks.lua
@@ -1,11 +1,20 @@
 function modApi:addModsInitializedHook(fn)
 	return modApi.events.onModsInitialized:subscribe(fn)
 end
+function modApi:remModsInitializedHook(fn)
+	return modApi.events.onModsInitialized:unsubscribe(fn)
+end
 
 function modApi:addModsLoadedHook(fn)
 	return modApi.events.onModsLoaded:subscribe(fn)
 end
+function modApi:remModsLoadedHook(fn)
+	return modApi.events.onModsLoaded:unsubscribe(fn)
+end
 
 function modApi:addModsFirstLoadedHook(fn)
 	return modApi.events.onModsFirstLoaded:subscribe(fn)
+end
+function modApi:remModsFirstLoadedHook(fn)
+	return modApi.events.onModsFirstLoaded:unsubscribe(fn)
 end
