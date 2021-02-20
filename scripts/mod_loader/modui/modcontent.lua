d5829dd4de0f56d720136b09a3ae7eddcf51c57e
kartoFlane <tbachminski@gmail.com>
2021-02-20 17:24:41 +0100

    Rebuild mod content dialog using dialog helpers

--- scripts/mod_loader/modui/modcontent.lua
+++ scripts/mod_loader/modui/modcontent.lua
@@ -3,7 +3,7 @@
 	an API for adding items to the menu it opens.
 --]]
 
-local modContent = {}
+local modContentList = {}
 function sdlext.addModContent(text, func, tip)
 	local obj = {
 		caption = text,
@@ -11,75 +11,87 @@ function sdlext.addModContent(text, func, tip)
 		tip = tip
 	}
 	
-	table.insert(modContent, obj)
+	table.insert(modContentList, obj)
 	
 	return obj
+end
+
+local function buildContent(scroll)
+	local holder = UiBoxLayout()
+		:vgap(12)
+		:width(1)
+		:addTo(scroll)
+
+	local buttonHeight = 42
+	for i = 1,#modContentList do
+		local modContent = modContentList[i]
+
+		local entryBtn = Ui()
+			:width(1)
+			:heightpx(buttonHeight)
+			:caption(modContent.caption)
+			:settooltip(modContent.tip)
+			:decorate({ DecoButton(), DecoAlign(0, 2), DecoCaption() })
+			:addTo(holder)
+
+		if modContent.disabled then entryBtn.disabled = true end
+
+		entryBtn.onclicked = function(self, button)
+			if button == 1 then
+				modContent.func()
+
+				return true
+			end
+
+			return false
+		end
+	end
+end
+
+local function showModContentDialog()
+	sdlext.showDialog(function(ui, quit)
+		local frame = sdlext.buildScrollDialog(
+			GetText("ModContent_FrameTitle"),
+			buildContent,
+			{
+				maxW = ScreenSizeX() * 0.4,
+				maxH = ScreenSizeY() * 0.8,
+				compactH = false
+			}
+		);
+
+		frame:addTo(ui)
+			 :pospx((ui.w - frame.w) / 2, (ui.h - frame.h) / 2)
+	end)
+end
+
+local function buildModContentButton()
+	local btn  = MainMenuButton("short")
+			:pospx(0, ScreenSizeY() - 186)
+			:caption(GetText("MainMenu_Button_ModContent"))
+	btn.visible = false
+
+	modApi.events.onGameWindowResized:subscribe(function(screen, oldSize)
+		btn:pospx(0, ScreenSizeY() - 186)
+	end)
+
+	btn.onclicked = function(self, button)
+		if button == 1 then
+			showModContentDialog()
+		end
+
+		return true
+	end
+
+	return btn
 end
 
 local buttonModContent
 modApi.events.onUiRootCreated:subscribe(function(screen, uiRoot)
 	if buttonModContent then return end
-	
-	buttonModContent = MainMenuButton("short")
-		:pospx(0, screen:h() - 186)
-		:caption(GetText("MainMenu_Button_ModContent"))
-		:addTo(uiRoot)
-	buttonModContent.visible = false
 
-	modApi.events.onGameWindowResized:subscribe(function(screen, oldSize)
-		buttonModContent:pospx(0, screen:h() - 186)
-	end)
-
-	buttonModContent.onclicked = function(self, button)
-		if button == 1 then
-			sdlext.showDialog(function(ui, quit)
-				local frame = Ui()
-					:width(0.4):height(0.8)
-					:posCentered()
-					:caption(GetText("ModContent_FrameTitle"))
-					:decorate({ DecoFrameHeader(), DecoFrame() })
-					:addTo(ui)
-
-				local scrollarea = UiScrollArea()
-					:width(1):height(1)
-					:padding(16)
-					:addTo(frame)
-
-				local holder = UiBoxLayout()
-					:vgap(12)
-					:width(1)
-					:addTo(scrollarea)
-				
-				local buttonHeight = 42
-				for i = 1,#modContent do
-					local obj = modContent[i]
-					local entryBtn = Ui()
-						:width(1)
-						:heightpx(buttonHeight)
-						:caption(obj.caption)
-						:settooltip(obj.tip)
-						:decorate({ DecoButton(), DecoAlign(0, 2), DecoCaption() })
-						:addTo(holder)
-
-					if obj.disabled then entryBtn.disabled = true end
-					
-					entryBtn.onclicked = function(self, button)
-						if button == 1 then
-							obj.func()
-
-							return true
-						end
-
-						return false
-					end
-				end
-			end)
-
-			return true
-		end
-
-		return false
-	end
+	buttonModContent = buildModContentButton()
+	buttonModContent:addTo(uiRoot);
 end)
 
 modApi.events.onMainMenuEntered:subscribe(function(screen, wasHangar, wasGame)
