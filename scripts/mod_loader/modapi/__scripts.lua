44cfb8b67fdc757750f94a8b414fe02014087229
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-03-11 21:18:38 +0100

    Added modApi/medals.lua
    Added code that will automatically track the highest difficulty victory for 2, 3 and 4 islands, for all modded squads.

--- scripts/mod_loader/modapi/__scripts.lua
+++ scripts/mod_loader/modapi/__scripts.lua
@@ -11,6 +11,7 @@ local scripts = {
 	"data",
 	"text",
 	"achievement",
+	"medals",
 	"palette",
 	"squad",
 	"drops",
---
+++ scripts/mod_loader/modapi/medals.lua
@@ -0,0 +1,186 @@
+
+-- use strings to make modcontent.lua easily readable for all users
+local ISLANDS = {
+	"2islands",
+	"3islands",
+	"4islands"
+}
+
+local DIFFICULTIES = {
+	"NONE",
+	"EASY",
+	"NORMAL",
+	"HARD",
+}
+
+local VANILLA_SQUADS = {
+	Archive_A = true,
+	Rust_A = true,
+	Pinnacle_A = true,
+	Detritus_A = true,
+	Archive_B = true,
+	Rust_B = true,
+	Pinnacle_B = true,
+	Detritus_B = true,
+	Secret = true
+}
+
+function modApi:isVanillaSquad(squad_id)
+	return VANILLA_SQUADS[squad_id] == true
+end
+
+function modApi:isModdedSquad(squad_id)
+	local squad = self.mod_squads_by_id[squad_id]
+	return squad ~= nil and not self:isVanillaSquad(squad_id)
+end
+
+-- returns the greatest difficulty of diffA and diffB
+local function getGreaterDifficulty(diffA, diffB)
+	local num_diffA = list_indexof(DIFFICULTIES, diffA)
+	local num_diffB = list_indexof(DIFFICULTIES, diffB)
+
+	if num_diffA > num_diffB then
+		return diffA
+	else
+		return diffB
+	end
+end
+
+local function pruneInvalidSquads(medals)
+	local rem = {}
+	for invalidSquadId, _ in pairs(medals) do
+		if VANILLA_SQUADS[invalidSquadId] ~= nil then
+			rem[#rem+1] = invalidSquadId
+		end
+	end
+
+	for _, invalidSquadId in ipairs(rem) do
+		medals[invalidSquadId] = nil
+	end
+end
+
+local function pruneInvalidMedals(squadMedals)
+	local rem = {}
+	for invalidIslandsSecured, invalidDifficulty in pairs(squadMedals) do
+		if
+			not list_contains(ISLANDS, invalidIslandsSecured)  or
+			not list_contains(DIFFICULTIES, invalidDifficulty)
+		then
+			rem[#rem+1] = invalidIslandsSecured
+		end
+	end
+
+	for _, invalidIslandsSecured in ipairs(rem) do
+		squadMedals[invalidIslandsSecured] = nil
+	end
+end
+
+local function validateAndCacheSaveData(squads_by_id)
+	sdlext.config(
+		modApi:getCurrentProfilePath().."modcontent.lua",
+		function(obj)
+			local medals = obj.medals or {}
+			obj.medals = medals
+			modApi.medals.cachedData = medals
+			squads_by_id = squads_by_id or medals
+			pruneInvalidSquads(medals)
+
+			for squad_id, _ in pairs(squads_by_id) do
+				if modApi:isModdedSquad(squad_id) then
+					local squadMedals = medals[squad_id] or {}
+					medals[squad_id] = squadMedals
+					pruneInvalidMedals(squadMedals)
+
+					for _, islandsSecured in ipairs(ISLANDS) do
+						squadMedals[islandsSecured] = squadMedals[islandsSecured] or DIFFICULTIES[1]
+					end
+				end
+			end
+		end
+	)
+end
+
+-- writes medal data to modcontent.lua
+local function writeMedalData(self, squad_id, difficulty, islandsSecured)
+	if
+		--not modApi:isProfilePath()         or
+		not modApi:isModdedSquad(squad_id) or
+		islandsSecured < 2                 or
+		difficulty < 0                     or
+		difficulty > 2
+	then
+		return
+	end
+
+	-- convert numerical island count and difficulty to
+	-- strings, in order to write human readable save
+	-- data to profile.
+	islandsSecured = ISLANDS[islandsSecured - 1]
+	difficulty = DIFFICULTIES[difficulty + 2]
+
+	-- validate and cache already saved medal data
+	if self.cachedData == nil then
+		validateAndCacheSaveData()
+	end
+
+	-- validate and cache medal data for new mod squads
+	if self.cachedData[squad_id] == nil then
+		validateAndCacheSaveData(modApi.mod_squads_by_id)
+	end
+
+	sdlext.config(
+		modApi:getCurrentProfilePath().."modcontent.lua",
+		function(obj)
+			local squadMedals = obj.medals[squad_id]
+
+			local currentCompletedDifficulty = squadMedals[islandsSecured]
+			squadMedals[islandsSecured] = getGreaterDifficulty(difficulty, currentCompletedDifficulty)
+			self.cachedData = obj.medals
+		end
+	)
+end
+
+-- reads medal data.
+local function readMedalData(self, squad_id)
+	if
+		--not modApi:isProfilePath()         or
+		not modApi:isModdedSquad(squad_id)
+	then
+		return nil
+	end
+
+	-- validate and cache already saved medal data
+	if self.cachedData == nil then
+		validateAndCacheSaveData()
+	end
+
+	-- validate and cache medal data for new mod squads
+	if self.cachedData[squad_id] == nil then
+		validateAndCacheSaveData(modApi.mod_squads_by_id)
+	end
+
+	return self.cachedData[squad_id]
+end
+
+modApi.events.onGameVictory:subscribe(function()
+	local difficulty = GetRealDifficulty()
+	local squad_id = GAME.additionalSquadData.squad
+	local islandsSecured = 0
+	for i = 0, 3 do
+		if RegionData["island"..i].secured then
+			islandsSecured = islandsSecured + 1
+		end
+	end
+
+	modApi.medals:writeData(squad_id, difficulty, islandsSecured)
+end)
+
+--[[modApi.events.onProfileChanged(function()
+	modApi.medals.cachedData = nil
+end)]]
+
+modApi.medals = {
+	cachedData = nil,
+	writeData = writeMedalData,
+	readData = readMedalData,
+}
