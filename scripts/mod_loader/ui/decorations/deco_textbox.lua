e84bb412ee470022861131833b01e2053954817b
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-06-06 11:56:46 +0200

    Updated deco_textbox.lua
    Changed decoration to not have separate build and draw steps.
    
    Modified methods related to rebuilding accordingly.
    
    Added methods:
    - caretToScreen - converts a caret position to screen coordinates.
    - screenToCaret - converts a screen coordinate to a caret position.
    - drawCaret - draws the caret at the widget's caret position.
    - onArrowUp - the widget's onArrowUp method is replaced and redirected to this method, which will convert an arrow up press to moving the caret position one line up.
    - onArrowDown - the widget's onArrowDown method is replaced and redirected to this method, which will convert an arrow down press to moving the caret position one line down.

--- scripts/mod_loader/ui/decorations/deco_textbox.lua
+++ scripts/mod_loader/ui/decorations/deco_textbox.lua
@@ -1,25 +1,7 @@
 --[[
-	A decoration used to display its widget's 'text' field as text,
-	within the widget's area. Created mainly for the UiInput Class,
-	in order to have text be dynamically created as the text changes.
-
-	The following actions rebuilds the text:
-	- edit widget.text
-	- edit widget.w
-	- edit widget.h
-	- call setFont
-	- call setTextSettings
-	- call setWrapText
-	- call setSplitWords
-	- call setPrefix
-	- call setSuffix
-	- set rebuild = true
-
-	The following actions updates the
-	displayed text without having to rebuild:
-	- edit alignH
-	- edit alignV
-	- edit lineSpacing
+	A decoration used to dynamically update and display
+	text contained in a widget.
+	Created mainly for the UiTextBox Class.
 --]]
 
 local surfaceFonts = {}
@@ -90,8 +72,8 @@ local function updateFont(self)
 	self.surfaceHeight = self.surfaceFont:get("|"):h()
 end
 
-DecoInput = Class.inherit(UiDeco)
-function DecoInput:new(opt)
+DecoTextBox = Class.inherit(UiDeco)
+function DecoTextBox:new(opt)
 	UiDeco.new(self)
 	opt = opt or {}
 	self.font = opt.font or deco.uifont.default.font
@@ -102,200 +84,291 @@ function DecoInput:new(opt)
 	self.alignV = opt.alignV or "center"
 	self.prefix = opt.prefix or ""
 	self.suffix = opt.suffix or ""
+	self.charSpacing = opt.charSpacing or 1
 	self.lineSpacing = opt.lineSpacing or 0
-	self.markerBlinkMs = opt.markerBlinkMs or 600
+	self.caretBlinkMs = opt.caretBlinkMs or 600
 	self.rect = sdl.rect(0,0,0,0)
-	self.lines = {}
 	updateFont(self)
 end
 
-function DecoInput:buildLines()
-	self.rebuild = false
-
-	if not self.width or not self.font or not self.textset or not self.text then
-		return
+function DecoTextBox:apply(widget)
+	widget.onArrowUp = function(widget)
+		self:onArrowUp(widget)
 	end
-
-	local text = self.prefix .. self.text .. self.suffix
-	local length = text:len()
-	local lines = {}
-	local word = {w = 0}
-	local line = {w = 0, c_w = 0}
-
-	local function commit(char)
-		word.w = word.w + char:w()
-		table.insert(word, char)
+	widget.onArrowDown = function(widget)
+		self:onArrowDown(widget)
 	end
-
-	local function newword()
-		line.w = line.w + word.w
-		for _, char in ipairs(word) do
-			table.insert(line, char)
-		end
-		word = {w = 0}
+	widget.mousedown = function(widget, mx, my, button)
+		self:mousedown(widget, mx, my, button)
+		return Ui.mousedown(widget, mx, my, button)
 	end
-
-	local function newline()
-		table.insert(lines, line)
-		line = {w = 0, c_w = 0}
-	end
-
-	for i = 1, length do
-		local str = text:sub(i,i)
-		local char_is_space = str:match("%s")
-		local char_is_enter = str:match("\n")
-		local char = self.surfaceFont:get(str)
-
-		if char_is_enter then
-			newword()
-			newline()
-		else
-			if self.wrapText and line.w + word.w + char:w() > self.width then
-				if line.w == 0 then
-					newword()
-				end
-				newline()
-			end
-
-			if self.wrapText and self.splitWords then
-				line.w = line.w + char:w()
-				table.insert(line, char)
-			else
-				if char_is_space then
-					newword()
-				end
-
-				commit(char)
-			end
-		end
-
-		if i == length then
-			newword()
-			newline()
-		end
-	end
-
-	self.lines = lines
 end
 
-function DecoInput:setFont(font)
+function DecoTextBox:unapply(widget)
+	-- TODO: check if this tracks
+	local class = getmetatable(widget).__index
+	widget.onArrowUp = class.onArrowUp
+	widget.onArrowDown = class.onArrowDown
+	widget.mousedown = class.mousedown
+end
+
+function DecoTextBox:onArrowUp(widget)
+	local x, y = self:caretToScreen(widget.caret)
+	y = y - (self.surfaceHeight - self.lineSpacing)
+	local caret = self:screenToCaret(x, y)
+	widget:setCaret(caret)
+end
+
+function DecoTextBox:onArrowDown(widget)
+	local x, y = self:caretToScreen(widget.caret)
+	y = y + (self.surfaceHeight - self.lineSpacing)
+	local caret = self:screenToCaret(x, y)
+	widget:setCaret(caret)
+end
+
+function DecoTextBox:mousedown(widget, mx, my, button)
+	local caret = self:screenToCaret(mx, my)
+	widget:setCaret(caret)
+end
+
+function DecoTextBox:setFont(font)
 	font = font or deco.uifont.default.font
 
 	if font ~= self.font then
 		self.font = font
 		updateFont(self)
-		self.rebuild = true
 	end
 end
 
-function DecoInput:setTextSettings(textset)
+function DecoTextBox:setTextSettings(textset)
 	textset = textset or deco.uifont.default.set
 
 	if textset ~= self.textset then
 		self.textset = textset
 		updateFont(self)
-		self.rebuild = true
 	end
 end
 
-function DecoInput:setWrapText(wrapText)
-	if wrapText ~= self.wrapText then
-		self.wrapText = wrapText
-		self.rebuild = true
+function DecoTextBox:setWrapText(wrapText)
+	if wrapText == nil then
+		wrapText = true
 	end
+
+	self.wrapText = wrapText
 end
 
-function DecoInput:setSplitWords(splitWords)
-	if splitWords ~= self.splitWords then
-		self.splitWords = splitWords
-		self.rebuild = true
+function DecoTextBox:setSplitWords(splitWords)
+	if splitWords == nil then
+		splitWords = true
 	end
+
+	self.splitWords = splitWords
 end
 
-function DecoInput:setPrefix(prefix)
-	if prefix ~= self.prefix then
-		self.prefix = prefix or ""
-		self.rebuild = true
-	end
+function DecoTextBox:setPrefix(prefix)
+	self.prefix = prefix or ""
 end
 
-function DecoInput:setSuffix(suffix)
-	if suffix ~= self.suffix then
-		self.suffix = suffix or ""
-		self.rebuild = true
-	end
+function DecoTextBox:setSuffix(suffix)
+	self.suffix = suffix or ""
 end
 
-function DecoInput:draw(screen, widget)
-	if widget.typedtext ~= self.text then
-		self.text = widget.typedtext
-		self.rebuild = true
-	end
+local linebuffer
+local linewidth
+local wordbuffer
+local wordwidth
+local linebuffers
+local linewidths
+local nullsurface = sdl.surface("")
 
-	if widget.w ~= self.width then
-		self.width = widget.w
-		self.rebuild = true
-	end
+local function newword()
+	linebuffer[#linebuffer+1] = wordbuffer
+	linewidth = linewidth + wordwidth
+	wordbuffer = {}
+	wordwidth = 0
+end
 
-	self.wasfocused = self.focused
-	self.focused = widget.focused
+local function newline()
+	linebuffers[#linebuffers+1] = linebuffer
+	linewidths[#linewidths+1] = linewidth
+	linebuffer = {}
+	linewidth = 0
+end
 
-	if self.rebuild then
-		self:buildLines()
-	end
+function DecoTextBox:caretToScreen(caret)
+	local x, y = 0, 0
 
-	local x = widget.screenx + widget.decorationx
-	local y = widget.screeny + widget.decorationy
-	local textHeight = self.surfaceHeight
-
-	if #self.lines > 1 then
-		textHeight = textHeight + (#self.lines - 1) * (self.surfaceHeight + self.lineSpacing)
-	end
-
-	if self.alignV == "bottom" then
-		y = y + widget.h - textHeight
-	elseif self.alignV == "center" then
-		y = y + math.floor(widget.h / 2 - textHeight / 2)
-	end
-
-	if self.alignH == "right" then
-		x = x + widget.w - 1
-	elseif self.alignH == "center" then
-		x = x + math.floor(widget.w / 2)
-	end
-
-	for l, line in ipairs(self.lines) do
-		x = widget.screenx + widget.decorationx
-
-		if self.alignH == "right" then
-			x = x + widget.w - line.w - 1
-		elseif self.alignH == "center" then
-			x = x + math.floor(widget.w / 2 - line.w / 2)
-		end
-
-		for c, char in ipairs(line) do
-			screen:blit(char, nil, x, y)
-			x = x + char:w()
-		end
-
-		if l ~= #self.lines then
-			y = y + self.surfaceHeight + self.lineSpacing
+	if self.coordsByChars then
+		local caretPosition = self.coordsByChars[caret]
+		if caretPosition then
+			x = caretPosition[1]
+			y = caretPosition[2]
 		end
 	end
 
-	if self.focused then
-		local time = os.clock() * 1000 % self.markerBlinkMs
+	return x, y
+end
 
-		if not self.wasfocused or not self.focustime then
+function DecoTextBox:screenToCaret(x, y)
+	local caret = 0
+
+	if self.coordsByLines and #self.coordsByLines > 0 then
+		local linebuffer_best = self.coordsByLines[1]
+
+		-- find best line
+		for line = 2, #self.textByLines do
+			local linebuffer = self.coordsByLines[line]
+			if linebuffer[1][2] > y then
+				break
+			end
+
+			linebuffer_best = linebuffer
+			caret = caret + #self.coordsByLines[line-1]
+		end
+
+		-- find best char
+		for char = 1, #linebuffer_best do
+			if linebuffer_best[char][1] > x then
+				break
+			end
+
+			caret = caret + 1
+		end
+	end
+
+	return caret
+end
+
+function DecoTextBox:drawCaret(screen, x, y)
+	self.rect.x = x
+	self.rect.y = y
+	self.rect.w = 1
+	self.rect.h = self.surfaceHeight
+	screen:drawrect(self.textset.color, self.rect)
+end
+
+function DecoTextBox:draw(screen, widget)
+	local focused = widget.focused
+	local caret = widget.caret
+	local isCaret = false
+	local focusChanged = focused ~= self.focused_prev
+	local caretChanged = caret ~= self.caret_prev
+
+	if focused then
+		local time = os.clock() * 1000 % self.caretBlinkMs
+
+		if not self.focustime or focusChanged or caretChanged then
 			self.focustime = time
 		end
 
-		if (time - self.focustime) % self.markerBlinkMs * 2 < self.markerBlinkMs then
-			self.rect.x = x
-			self.rect.y = y
-			self.rect.w = 1
-			self.rect.h = self.surfaceHeight
-			screen:drawrect(self.textset.color, self.rect)
+		if (time - self.focustime) % self.caretBlinkMs * 2 < self.caretBlinkMs then
+			isCaret = true
 		end
+	end
+
+	self.focused_prev = focused
+	self.caret_prev = caret
+
+	linebuffer = {}
+	linewidth = 0
+	wordbuffer = {}
+	wordwidth = 0
+	linebuffers = {}
+	linewidths = {}
+
+	self.linebuffers = linebuffers
+	self.linewidths = linewidths
+
+	local text = self.prefix..widget.typedtext..self.suffix
+	local length = text:len()
+	for i = 1, length do
+		local char = text:sub(i,i)
+
+		if char == "\n" then
+			newword()
+			newline()
+			wordbuffer[#wordbuffer+1] = nullsurface
+		else
+			local surf = self.surfaceFont:get(char)
+			local surfwidth = surf:w() + self.charSpacing
+
+			if self.wrapText then
+				if wordwidth + surfwidth >= widget.w then
+					newword()
+					newline()
+				end
+			end
+
+			wordbuffer[#wordbuffer+1] = surf
+			wordwidth = wordwidth + surfwidth
+
+			if self.wrapText and not self.splitWords then
+				if linewidth + wordwidth >= widget.w then
+					newline()
+				end
+
+				if char == " " then
+					newword()
+				end
+			end
+		end
+	end
+
+	if #wordbuffer > 0 then
+		newword()
+	end
+
+	if #linebuffer > 0 then
+		newline()
+	end
+
+	local x = widget.screenx + widget.decorationx + widget.dx
+	local y = widget.screeny + widget.decorationy + widget.dy
+	local textheight = #linebuffers * (self.surfaceHeight + self.lineSpacing) - self.lineSpacing
+	local coordsByLines = {}
+	local coordsByChars = { [0] = { x, y } }
+
+	if self.alignV == "bottom" then
+		y = y + widget.h - textheight
+	elseif self.alignV == "center" then
+		y = y + math.floor(widget.h / 2 - textheight / 2)
+	end
+
+	for line = 1, #linebuffers do
+		local linebuffer = linebuffers[line]
+		local linewidth = linewidths[line]
+		coordsByLines[line] = {}
+
+		if self.alignH == "right" then
+			x = x + widget.w - linewidth
+		elseif self.alignH == "center" then
+			x = x + math.floor((widget.w - linewidth) / 2)
+		end
+
+		for word = 1, #linebuffer do
+			local wordbuffer = linebuffer[word]
+
+			for _, surf in ipairs(wordbuffer) do
+				local w = surf:w()
+				if w > 0 then
+					screen:blit(surf, nil, x, y)
+					x = x + w + self.charSpacing
+				end
+
+				local coords = { x, y }
+				coordsByChars[#coordsByChars + 1] = coords
+				coordsByLines[line][#coordsByLines[line] + 1] = coords
+			end
+		end
+
+		x = widget.screenx + widget.decorationx
+		y = y + self.surfaceHeight + self.lineSpacing
+	end
+
+	self.coordsByChars = coordsByChars
+	self.coordsByLines = coordsByLines
+
+	if isCaret then
+		self:drawCaret(screen, self:caretToScreen(caret))
 	end
 end
