2f7b124c4b64b38df1f6e6163a3ea16ff772f4e0
kartoFlane <tbachminski@gmail.com>
2021-07-25 03:20:51 +0200

    Initialize mouseTile-related function in a deferred way
    The way modApiExt registers its own versions of these functions means
    we have to redefine them each time mods are loaded.

--- scripts/mod_loader/modapi/global.lua
+++ scripts/mod_loader/modapi/global.lua
@@ -77,131 +77,136 @@ GetUiScaleY = function()
 	return 1
 end
 
---[[
-	Returns currently highlighted board tile, or nil.
---]]
-function mouseTile()
-	-- Use custom table instead of the existing Point class, since Point
-	-- can only hold integer values and automatically rounds them.
-	return screenPointToTile({ x = sdl.mouse.x(), y = sdl.mouse.y() }, false)
-end
-
---[[
-	Returns currently highlighted board tile and closest tile edge as DIR, or nil.
---]]
-function mouseTileAndEdge()
-	return screenPointToTile({ x = sdl.mouse.x(), y = sdl.mouse.y() }, true)
-end
-
-function getScreenRefs(scale)
-	scale = scale or GetBoardScale()
-
-	local tw = 28
-	local th = 21
-
-	-- Top corner of the (0, 0) tile
-	local tile00 = {
-		x = screen:w() / 2,
-		y = screen:h() / 2 - 8 * th * scale
-	}
-
-	tile00.y = tile00.y + (30 - 10 * scale)
-
-	local lineX = function(x) return x * th/tw end
-	local lineY = function(x) return -lineX(x) end
-
-	return tile00, lineX, lineY, tw, th
-end
-
-local function computeTile(tilex, tiley, tilew, tileh, sourcePointBoardSpace)
-	return {
-		x = sourcePointBoardSpace.x - tilew * (tilex - tiley),
-		y = sourcePointBoardSpace.y - tileh * (tilex + tiley)
-	}
-end
-
-local function isPointAboveLine(point, lineFn)
-	return point.y >= lineFn(point.x)
-end
-
-local function tileContains(tile, lineX, lineY)
-	return isPointAboveLine(tile, lineX)
-		and isPointAboveLine(tile, lineY)
-end
-
-local function computeClosestTileEdge(sourcePointTileSpaceTop, tileh)
-	local sourcePointTileSpaceCenter = {
-		x = sourcePointTileSpaceTop.x,
-		y = sourcePointTileSpaceTop.y - tileh
-	}
-
-	if
-		sourcePointTileSpaceCenter.x >= 0 and
-		sourcePointTileSpaceCenter.y <  0
-	then
-		return DIR_UP
-	elseif
-		sourcePointTileSpaceCenter.x >= 0 and
-		sourcePointTileSpaceCenter.y >= 0
-	then
-		return DIR_RIGHT
-	elseif
-		sourcePointTileSpaceCenter.x <  0 and
-		sourcePointTileSpaceCenter.y >= 0
-	then
-		return DIR_DOWN
-	else
-		return DIR_LEFT
+initializeMouseTile = function()
+	--[[
+		Returns currently highlighted board tile, or nil.
+	--]]
+	function mouseTile()
+		-- Use custom table instead of the existing Point class, since Point
+		-- can only hold integer values and automatically rounds them.
+		return screenPointToTile({ x = sdl.mouse.x(), y = sdl.mouse.y() }, false)
 	end
-end
 
---[[
-	Returns a board tile at the specified point on the screen, or nil.
---]]
-function screenPointToTile(sourcePointScreenSpace, findTileEdge)
-	if not Board then return nil end
+	--[[
+		Returns currently highlighted board tile and closest tile edge as DIR, or nil.
+	--]]
+	function mouseTileAndEdge()
+		return screenPointToTile({ x = sdl.mouse.x(), y = sdl.mouse.y() }, true)
+	end
 
-	local scale = GetBoardScale()
+	function getScreenRefs(scale)
+		scale = scale or GetBoardScale()
 
-	local tile00, lineX, lineY, tw, th = getScreenRefs(scale)
+		local tw = 28
+		local th = 21
 
-	-- Change sourcePointScreenSpace to be relative to the (0, 0) tile
-	-- and move to unscaled space.
-	local sourcePointBoardSpace = {
-		x = (sourcePointScreenSpace.x - tile00.x) / scale,
-		y = (sourcePointScreenSpace.y - tile00.y) / scale
-	}
+		-- Top corner of the (0, 0) tile
+		local tile00 = {
+			x = screen:w() / 2,
+			y = screen:h() / 2 - 8 * th * scale
+		}
 
-	-- Start at the end of the board and move backwards.
-	-- That way we only need to check 2 lines instead of 4 on each tile.
-	-- The tradeoff is that we need to check an additional row and column
-	-- of tiles outside of the board.
-	local bsize = Board:GetSize()
-	for tileY = bsize.y, 0, -1 do
-		for tileX = bsize.x, 0, -1 do
-			local tile = computeTile(tileX, tileY, tw, th, sourcePointBoardSpace)
-			if tileContains(tile, lineX, lineY) then
-				if tileY == bsize.y or tileX == bsize.x then
-					-- outside of the board
-					return nil
-				end
+		tile00.y = tile00.y + (30 - 10 * scale)
 
-				if findTileEdge then
-					local closestTileEdge = computeClosestTileEdge(tile, th, sourcePointBoardSpace)
-				
-					return Point(tileX, tileY), closestTileEdge
-				else
-					return Point(tileX, tileY)
-				end
-			end
+		local lineX = function(x) return x * th/tw end
+		local lineY = function(x) return -lineX(x) end
+
+		return tile00, lineX, lineY, tw, th
+	end
+
+	local function computeTile(tilex, tiley, tilew, tileh, sourcePointBoardSpace)
+		return {
+			x = sourcePointBoardSpace.x - tilew * (tilex - tiley),
+			y = sourcePointBoardSpace.y - tileh * (tilex + tiley)
+		}
+	end
+
+	local function isPointAboveLine(point, lineFn)
+		return point.y >= lineFn(point.x)
+	end
+
+	local function tileContains(tile, lineX, lineY)
+		return isPointAboveLine(tile, lineX)
+			and isPointAboveLine(tile, lineY)
+	end
+
+	local function computeClosestTileEdge(sourcePointTileSpaceTop, tileh)
+		local sourcePointTileSpaceCenter = {
+			x = sourcePointTileSpaceTop.x,
+			y = sourcePointTileSpaceTop.y - tileh
+		}
+
+		if
+			sourcePointTileSpaceCenter.x >= 0 and
+			sourcePointTileSpaceCenter.y <  0
+		then
+			return DIR_UP
+		elseif
+			sourcePointTileSpaceCenter.x >= 0 and
+			sourcePointTileSpaceCenter.y >= 0
+		then
+			return DIR_RIGHT
+		elseif
+			sourcePointTileSpaceCenter.x <  0 and
+			sourcePointTileSpaceCenter.y >= 0
+		then
+			return DIR_DOWN
+		else
+			return DIR_LEFT
 		end
 	end
 
-	return nil
+	--[[
+		Returns a board tile at the specified point on the screen, or nil.
+	--]]
+	function screenPointToTile(sourcePointScreenSpace, findTileEdge)
+		if not Board then return nil end
+
+		local scale = GetBoardScale()
+
+		local tile00, lineX, lineY, tw, th = getScreenRefs(scale)
+
+		-- Change sourcePointScreenSpace to be relative to the (0, 0) tile
+		-- and move to unscaled space.
+		local sourcePointBoardSpace = {
+			x = (sourcePointScreenSpace.x - tile00.x) / scale,
+			y = (sourcePointScreenSpace.y - tile00.y) / scale
+		}
+
+		-- Start at the end of the board and move backwards.
+		-- That way we only need to check 2 lines instead of 4 on each tile.
+		-- The tradeoff is that we need to check an additional row and column
+		-- of tiles outside of the board.
+		local bsize = Board:GetSize()
+		for tileY = bsize.y, 0, -1 do
+			for tileX = bsize.x, 0, -1 do
+				local tile = computeTile(tileX, tileY, tw, th, sourcePointBoardSpace)
+				if tileContains(tile, lineX, lineY) then
+					if tileY == bsize.y or tileX == bsize.x then
+						-- outside of the board
+						return nil
+					end
+
+					if findTileEdge then
+						local closestTileEdge = computeClosestTileEdge(tile, th, sourcePointBoardSpace)
+
+						return Point(tileX, tileY), closestTileEdge
+					else
+						return Point(tileX, tileY)
+					end
+				end
+			end
+		end
+
+		return nil
+	end
 end
 
---initializeMouseTile()
---modApi.events.onModsLoaded:subscribe(initializeMouseTile)
+initializeMouseTile()
+-- Setup mouseTile-related functions again each time mods are loaded,
+-- since every version of modApiExt overwrites these globals on init,
+-- and then most recent on each load.
+modApi.events.onModsLoaded:subscribe(initializeMouseTile)
 
 function isUserdataPoint(var)
 	return type(var) == 'userdata' and type(var.x) == 'number' and type(var.y) == 'number'
