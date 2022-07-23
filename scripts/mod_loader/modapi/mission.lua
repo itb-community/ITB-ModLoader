991b5acce04b9ed63d07bfdb22f0f50a9b923520
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-07-23 21:59:53 +0200

    Preliminary change for GetCurrentMission
    w/tests

--- scripts/mod_loader/modapi/mission.lua
+++ scripts/mod_loader/modapi/mission.lua
@@ -1,4 +1,42 @@
 
+-- TESTS
+--------
+
+-- Logout when mission changes.
+modApi.events.onMissionChanged:subscribe(function(currentMission, oldCurrentMission)
+	local oldId = oldCurrentMission and oldCurrentMission.ID or nil
+	local newId = currentMission and currentMission.ID or nil
+
+	LOGF("-> Current mission changed from %s w/table [%s] to %s w/table [%s]",
+		tostring(oldId),
+		tostring(oldCurrentMission),
+		tostring(newId),
+		tostring(currentMission)
+	)
+end)
+
+-- Test if GetCurrentMission() is equal to 'self' in every Mission method.
+for i, fn in pairs(Mission) do
+	if type(fn) == 'function' then
+		local oldfn = fn
+		fn = function(mission, ...)
+			if GetCurrentMission() ~= mission then
+				LOGF("-> Mission:%s: GetCurrentMission() (%s) differs from 'self' (%s)",
+					tostring(i),
+					tostring(GetCurrentMission()),
+					tostring(mission)
+				)
+			end
+
+			return oldfn(mission, ...)
+		end
+	end
+end
+
+
+-- GetCurrentMission
+--------------------
+
 function modApi:setMission(mission)
 	local oldMission = self.current_mission
 
@@ -9,38 +47,22 @@ function modApi:setMission(mission)
 	end
 end
 
-local currentRegionData
-modApi.events.onSaveDataUpdated:subscribe(function()
-	if currentRegionData ~= RegionData then
-		currentRegionData = RegionData
-
-		local region = GetCurrentRegion(RegionData)
-		local mission = nil
-
-		if region then
-			mission = GAME:GetMission(region.mission)
-		end
-
-		if not region and modApi.current_mission and not modApi.current_mission.Deployed then
-			-- When updating the save file in MissionStart event listener (ie, from
-			-- Mission:BaseDeployment context), RegionData doesn't have `iBattleRegion` entry yet,
-			-- so we have no way of finding the current region/mission, and end up setting current
-			-- mission to nil.
-			-- To fix this, keep track of missions that are past the deployment phase with `Deployed`
-			-- flag, and check if the mission we have saved currently is already past that phase.
-			-- This way we know that we've been called from within BaseDeployment context, and don't
-			-- execute this block eg. when leaving a mission, which would be incorrect behaviour.
-			mission = modApi.current_mission
-		end
-
-		if not IsTestMechScenario() then
-			modApi:setMission(mission)
-		end
-	end
+modApi.events.onMissionStart:subscribe(function(mission)
+	modApi:setMission(mission)
 end)
 
 modApi.events.onMissionNextPhaseCreated:subscribe(function(prevMission, nextMission)
 	modApi:setMission(nextMission)
+end)
+
+modApi.events.onMissionEnd:subscribe(function(mission)
+	modApi:setMission(nil)
+end)
+
+modApi.events.onMissionUpdate:subscribe(function(mission)
+	if currentMission ~= mission then
+		modApi:setMission(mission)
+	end
 end)
 
 modApi.events.onGameExited:subscribe(function()
