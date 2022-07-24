e62d52076f148e62588102fe63687da12a5233cb
kartoFlane <tbachminski@gmail.com>
2022-07-24 16:57:38 +0200

    Nuke the part of FURL responsible for handling palettes

--- scripts/mod_loader/compat/furl.lua
+++ scripts/mod_loader/compat/furl.lua
@@ -13,51 +13,34 @@ end
 modApi.events.onModEnumerating:subscribe(function(modDir)
     local modScriptPath = modDir .. "scripts/"
     if not isModLoaderFURLCompatApplied(modScriptPath) and isFURLMod(modScriptPath) then
-        modApi:writeFile(
-            modScriptPath .. "FURL.lua",
-            [[
-return function(mod, table)
-    HandleFURLCall(mod, table)
-end
-            ]]
+        -- It seems that the only problematic part in FURL is in how it handles palettes,
+        -- so we only replace that part, instead of nuking it in full.
+        ReplaceFileContent(
+            modScriptPath.."FURL.lua",
+            "\tloadColors%(mod, object%)",
+            "\tHandleFURLColor(mod, object)"
         )
 
         modApi:writeFile(modScriptPath .. ".furl-compat", "")
     end
 end)
 
-local function handleEnemy(mod, entry)
-end
-
-local function handleMech(mod, entry)
-end
-
-local function handleAnimation(mod, entry)
-end
-
-local function handleBase(mod, entry)
-end
-
 local function handleColor(mod, entry)
+    modApi:addPalette({
+        name = entry.Name,
+        colorMap = {
+            PlateHighlight = entry.PlateHighlight,
+            PlateLight = entry.PlateLight,
+            PlateMid = entry.PlateMid,
+            PlateDark = entry.PlateDark,
+            PlateOutline = entry.PlateOutline,
+            PlateShadow = entry.PlateShadow,
+            BodyColor = entry.BodyColor,
+            BodyHighlight = entry.BodyHighlight
+        }
+    })
+
+    FURL_COLORS[entry.Name] = GetColorCount()
 end
 
-local function handleFURLCall(mod, table)
-    for i, entry in ipairs(table) do
-        local type = entry.Type or "null"
-        if type == "enemy" then
-            handleEnemy(mod, entry)
-        elseif type == "mech" then
-            handleMech(mod, entry)
-        elseif type == "anim" then
-            handleAnimation(mod, entry)
-        elseif type == "base" then
-            handleBase(mod, entry)
-        elseif type == "color" then
-            handleColor(mod, entry)
-        else
-            LOG("Error: missing or invalid type value")
-        end
-    end
-end
-
-HandleFURLCall = handleFURLCall
+HandleFURLColor = handleColor
