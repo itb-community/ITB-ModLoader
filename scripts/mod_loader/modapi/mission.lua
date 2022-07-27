3d35ff5129527a52fb920fd0543d85a07293984a
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-07-27 09:21:44 +0200

    Removed GetCurrentMission debug test code

--- scripts/mod_loader/modapi/mission.lua
+++ scripts/mod_loader/modapi/mission.lua
@@ -9,47 +9,6 @@ modApi.events.onFrameDrawStart:subscribe(function()
 end)
 
 
--- TESTS
---------
-
--- Logout when mission changes.
-modApi.events.onMissionChanged:subscribe(function(currentMission, oldCurrentMission)
-	local oldId = oldCurrentMission and oldCurrentMission.ID or nil
-	local newId = currentMission and currentMission.ID or nil
-
-	LOGF("-> Current mission changed from %s w/table [%s] to %s w/table [%s]",
-		tostring(oldId),
-		tostring(oldCurrentMission),
-		tostring(newId),
-		tostring(currentMission)
-	)
-end)
-
-modApi.events.onMissionDismissed:subscribe(function(mission)
-	local id = mission and mission.ID or nil
-
-	LOGF("-> Current mission %s w/table [%s] was dismissed", tostring(id), tostring(mission))
-end)
-
--- Test if GetCurrentMission() is equal to 'self' in every Mission method.
-for i, fn in pairs(Mission) do
-	if type(fn) == 'function' then
-		local oldfn = fn
-		fn = function(mission, ...)
-			if GetCurrentMission() ~= mission then
-				LOGF("-> Mission:%s: GetCurrentMission() (%s) differs from 'self' (%s)",
-					tostring(i),
-					tostring(GetCurrentMission()),
-					tostring(mission)
-				)
-			end
-
-			return oldfn(mission, ...)
-		end
-	end
-end
-
-
 -- GetCurrentMission
 --------------------
 
