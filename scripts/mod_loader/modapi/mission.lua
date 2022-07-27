02b3c54f70369fbd09d0ccadadb2ddbcf79ef4a9
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-07-27 09:39:45 +0200

    Change for GetCurrentMission to be more stable (#131)
    Create event onMissionDismissed
    Nil mission when dismissed

--- scripts/mod_loader/modapi/mission.lua
+++ scripts/mod_loader/modapi/mission.lua
@@ -1,4 +1,17 @@
 
+-- EVENTS
+---------
+
+modApi.events.onFrameDrawStart:subscribe(function()
+	if modApi.current_mission ~= nil and Board == nil then
+		modApi.events.onMissionDismissed:dispatch(modApi.current_mission)
+	end
+end)
+
+
+-- GetCurrentMission
+--------------------
+
 function modApi:setMission(mission)
 	local oldMission = self.current_mission
 
@@ -9,38 +22,28 @@ function modApi:setMission(mission)
 	end
 end
 
-local currentRegionData
-modApi.events.onSaveDataUpdated:subscribe(function()
-	if currentRegionData ~= RegionData then
-		currentRegionData = RegionData
-
-		local region = GetCurrentRegion(RegionData)
-		local mission = nil
-
-		if region then
-			mission = GAME:GetMission(region.mission)
-		end
-
-		if not region and modApi.current_mission and not modApi.current_mission.Deployed then
-			-- When updating the save file in MissionStart event listener (ie, from
-			-- Mission:BaseDeployment context), RegionData doesn't have `iBattleRegion` entry yet,
-			-- so we have no way of finding the current region/mission, and end up setting current
-			-- mission to nil.
-			-- To fix this, keep track of missions that are past the deployment phase with `Deployed`
-			-- flag, and check if the mission we have saved currently is already past that phase.
-			-- This way we know that we've been called from within BaseDeployment context, and don't
-			-- execute this block eg. when leaving a mission, which would be incorrect behaviour.
-			mission = modApi.current_mission
-		end
-
-		if not IsTestMechScenario() then
-			modApi:setMission(mission)
-		end
-	end
+modApi.events.onMissionStart:subscribe(function(mission)
+	modApi:setMission(mission)
 end)
 
 modApi.events.onMissionNextPhaseCreated:subscribe(function(prevMission, nextMission)
 	modApi:setMission(nextMission)
+end)
+
+modApi.events.onMissionEnd:subscribe(function(mission)
+	-- When entering a saved game from the main menu,
+	-- this is the first function being called for the current mission
+	modApi:setMission(mission)
+end)
+
+modApi.events.onMissionUpdate:subscribe(function(mission)
+	if currentMission ~= mission then
+		modApi:setMission(mission)
+	end
+end)
+
+modApi.events.onMissionDismissed:subscribe(function(mission)
+	modApi:setMission(nil)
 end)
 
 modApi.events.onGameExited:subscribe(function()
