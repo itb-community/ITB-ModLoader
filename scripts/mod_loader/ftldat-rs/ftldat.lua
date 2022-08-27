2d3a878adcc73c54d688a185dc5438e7c55f2ff2
kartoFlane <tbachminski@gmail.com>
2022-08-27 04:34:33 +0200

    Improve error handling, actually log errors when ftldat fails

--- scripts/mod_loader/ftldat-rs/ftldat.lua
+++ scripts/mod_loader/ftldat-rs/ftldat.lua
@@ -5,15 +5,19 @@ local function lazy_load()
 		return
 	end
 
-	LOG("Loading ftldat.dll...")
-	local ok, err = pcall(package.loadlib("ftldat.dll", "luaopen_ftldat"))
-	if not ok then
-		error(string.format("Failed to load ftldat.dll: %s", err))
-	else
+	try(function()
+		LOG("Loading ftldat.dll...")
+		package.loadlib("ftldat.dll", "luaopen_ftldat")()
+		ftldat_rs = ftldat
+		ftldat = nil
 		LOG("Successfully loaded ftldat.dll!")
-	end
-	ftldat_rs = ftldat
-	ftldat = nil
+	end)
+	:catch(function(err)
+		error(string.format(
+				"Failed to load ftldat.dll: %s",
+				tostring(err)
+		))
+	end)
 end
 
 -- Use a class to serve as a wrapper around the Package userdata returned by ftldat-rs,
@@ -26,46 +30,106 @@ function FtlDat:new(filename)
 	if filename == nil then
 		self.package = ftldat_rs.new_package()
 	else
-		self.package = ftldat_rs.read_package(filename)
-		self.signature = self.package:exists(modApi:getSignature())
+		try(function()
+			self.package = ftldat_rs.read_package(filename)
+			self.signature = self.package:exists(modApi:getSignature())
+		end)
+		:catch(function(err)
+			error(string.format(
+					"Failed to create FtlDat package for file '%s'. Does it exist?\n%s",
+					filename, tostring(err)
+			))
+		end)
 	end
 end
 
 function FtlDat:remove_all_files()
+	Assert.Equals("table", type(self))
 	self.package:clear()
 end
 
 function FtlDat:write(outputPath)
-	self.package:to_file(outputPath)
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(outputPath))
+
+	try (function()
+		self.package:to_file(outputPath)
+	end)
+	:catch(function(err)
+		error(string.format(
+				"Failed to write ftldat archive to %s\n%s",
+				outputPath, tostring(err)
+		))
+	end)
 end
 
 function FtlDat:put_entry_string(innerPath, content)
-	if type(content) == "string" then
-		-- Sending some strings back to ftldat-rs causes an access violation when the string represents
-		-- content of a file that is typically binary (eg. images or fonts).
-		-- Work around this by communicating with ftldat-rs using byte arrays.
-		content = { content:byte(1, -1) }
-	end
-	Assert.Equals("table", type(content))
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(innerPath))
+	Assert.Equals("string", type(content))
 
-	self.package:put_entry_from_byte_array(innerPath, content)
+	try(function()
+		self.package:put_entry_from_string(innerPath, content)
+	end)
+	:catch(function(err_string)
+		try(function()
+			self.package:put_entry_from_byte_array(innerPath, content)
+		end)
+		:catch(function(err_byte_array)
+			error(string.format(
+					"Failed to put entry '%s' content from string\nas string: %s\nas byte array: %s",
+					innerPath, tostring(err_string), tostring(err_byte_array)
+			))
+		end)
+	end)
 end
 
 function FtlDat:file_exists(name)
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(name))
 	return self.package:exists(name)
 end
 
 --- Returns content of the specified file, interpreted as text.
 function FtlDat:entry_content_string(innerPath)
-	return self.package.read_content_as_string(innerPath)
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(innerPath))
+
+	local result
+	try(function()
+		result = self.package:read_content_as_string(innerPath)
+	end)
+	:catch(function(err)
+		error(string.format(
+				"Failed to read entry '%s' content as string\n%s",
+				innerPath, tostring(err)
+		))
+	end)
+
+	return result
 end
 
 --- Returns content of the specified file as an array of bytes.
 function FtlDat:entry_content_byte_array(innerPath)
-	return self.package:read_content_as_byte_array(innerPath)
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(innerPath))
+
+	local result
+	try(function()
+		result = self.package:read_content_as_byte_array(innerPath)
+	end)
+	:catch(function(err)
+		error(string.format(
+				"Failed to read entry '%s' content as byte array\n%s",
+				innerPath, tostring(err)
+		))
+	end)
+
+	return result
 end
 
 function FtlDat:inner_paths()
+	Assert.Equals("table", type(self))
 	return self.package:inner_paths()
 end
 
@@ -80,12 +144,24 @@ end
 
 --- Extract the specified file from .dat and save it to the specified destination.
 function FtlDat:extract_file(innerPath, destinationPath)
-	local content = self.package:read_content_as_byte_array(innerPath)
-	content = utf8_from(content)
+	Assert.Equals("table", type(self))
+	Assert.Equals("string", type(innerPath))
+	Assert.Equals("string", type(destinationPath))
 
-	local f = assert(io.open(destinationPath, "wb"), destinationPath)
-	f:write(content)
-	f:close()
+	try(function()
+		local content = self.entry_content_byte_array(innerPath)
+		content = utf8_from(content)
+
+		local f = assert(io.open(destinationPath, "wb"), destinationPath)
+		f:write(content)
+		f:close()
+	end)
+	:catch(function(err)
+		error(string.format(
+				"Failed to extract '%s' from ftldat archive to '%s'\n%s",
+				innerPath, destinationPath, tostring(err)
+		))
+	end)
 end
 
 function FtlDat:destroy()
