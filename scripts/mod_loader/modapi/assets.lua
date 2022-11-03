a76d16ed0de292c7dcc962ae33c3282200632a97
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-03 21:14:12 +0100

    Validate arguments in appendAssets
    Add general function `modApi:appendAssets` which appends all assets in a mod specified mod folder to a specified folder in resource.dat..
    
    All other append-Assets functions now also validate arguments as they are calling the general appendAssets internally.
    
    prefix string is now optional

--- scripts/mod_loader/modapi/assets.lua
+++ scripts/mod_loader/modapi/assets.lua
@@ -3,53 +3,59 @@ local function relpath(dir)
 	return dir:path():gsub("^"..Directory():path(), "")
 end
 
-local function appendAssets(gameRoot, modPath, prefix)
+function modApi:appendAssets(gameRoot, modPath, prefix)
+	Assert.Equals("table", type(self), "Check for . vs :")
 	Assert.ResourceDatIsOpen("Unable to append assets after init")
+	Assert.Equals("string", type(gameRoot), "Argument #1")
+	Assert.Equals("string", type(modPath), "Argument #2")
+	Assert.Equals({"nil", "string"}, type(prefix), "Argument #3")
 
-	local modDir = Directory(modApi:getCurrentMod().resourcePath, modPath)
+	prefix = prefix or ""
+
+	local modDir = Directory(self:getCurrentMod().resourcePath, modPath)
 	local modRoot = relpath(modDir)
 	for _, file in ipairs(modDir:files()) do
 		local filename = file:name()
 		if filename:find(".png$") then
-			modApi:appendAsset(gameRoot..prefix..filename, modRoot..filename)
+			self:appendAsset(gameRoot..prefix..filename, modRoot..filename)
 		end
 	end
 end
 
 function modApi:appendPlayerUnitAssets(path, prefix)
-	appendAssets("img/units/player/", path, prefix)
+	self:appendAssets("img/units/player/", path, prefix)
 end
 
 function modApi:appendEnemyUnitAssets(path, prefix)
-	appendAssets("img/units/aliens/", path, prefix)
+	self:appendAssets("img/units/aliens/", path, prefix)
 end
 
 function modApi:appendMissionUnitAssets(path, prefix)
-	appendAssets("img/units/mission/", path, prefix)
+	self:appendAssets("img/units/mission/", path, prefix)
 end
 
 function modApi:appendBotUnitAssets(path, prefix)
-	appendAssets("img/units/snowbots/", path, prefix)
+	self:appendAssets("img/units/snowbots/", path, prefix)
 end
 
 function modApi:appendCombatAssets(path, prefix)
-	appendAssets("img/combat/", path, prefix)
+	self:appendAssets("img/combat/", path, prefix)
 end
 
 function modApi:appendCombatIconAssets(path, prefix)
-	appendAssets("img/combat/icons/", path, prefix)
+	self:appendAssets("img/combat/icons/", path, prefix)
 end
 
 function modApi:appendEffectAssets(path, prefix)
-	appendAssets("img/effects/", path, prefix)
+	self:appendAssets("img/effects/", path, prefix)
 end
 
 function modApi:appendWeaponAssets(path, prefix)
-	appendAssets("img/weapons/", path, prefix)
+	self:appendAssets("img/weapons/", path, prefix)
 end
 
 function modApi:appendPassiveWeaponAssets(path, prefix)
-	appendAssets("img/weapons/passives/", path, prefix)
+	self:appendAssets("img/weapons/passives/", path, prefix)
 end
 
 modApi.appendMechAssets = modApi.appendPlayerUnitAssets
