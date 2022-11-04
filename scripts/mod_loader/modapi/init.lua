f12b1ca8903ff83e2b0a53d723a8d556b23dbd24
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-05 00:59:28 +0100

    Mod Loader v2.8.0

--- scripts/mod_loader/modapi/init.lua
+++ scripts/mod_loader/modapi/init.lua
@@ -15,7 +15,7 @@ function modApi:init()
 	-- if we start logging before we apply the config, we may not update the log file correctly
 	ApplyModLoaderConfig(LoadModLoaderConfig())
 
-	self.version = "2.7.3"
+	self.version = "2.8.0"
 	LOG("GAME VERSION", self:getGameVersion())
 	LOG("MOD-API VERSION", self.version)
 	LOGD("Parent directory:", parentDirectory)
