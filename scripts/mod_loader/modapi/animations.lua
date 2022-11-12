d1b1b82b4214984ebcb868270cdecf24e6e8ed59
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-12 19:28:05 +0100

    Add modApi.createVekAnimations
    This function is similar to `modApi.createMechAnimations`, but targets images in "img/units/aliens/" and uses base animation `EnemyUnit` by default

--- scripts/mod_loader/modapi/animations.lua
+++ scripts/mod_loader/modapi/animations.lua
@@ -1,67 +1,78 @@
 
 local IMG = "img/"
 local ROOT_MECH = "units/player/"
+local ROOT_VEK = "units/aliens/"
 local PNG = ".png"
 local TILE_HALF_HEIGHT = 21
+
+local function createAnimation(id, animDef)
+	if type(animDef.Image) ~= 'string' then
+		Assert.Error("Animation 'Image' must be a 'string'")
+	end
+
+	local filePath = animDef.Image
+	local fullFilePath = IMG..filePath
+
+	if not modApi:assetExists(fullFilePath) then
+		Assert.Error(string.format("Asset %q not found in resource.dat", fullFilePath))
+	end
+
+	local base = animDef.Base or "Animation"
+	local baseAnim = ANIMS[base]
+
+	if baseAnim == nil then
+		Assert.Error(string.format("Base animation %s not found", base))
+	end
+
+	local anim = baseAnim:new(animDef)
+
+	if anim.CenterX then
+		anim.PosX = -anim.CenterX
+	end
+
+	if anim.CenterY then
+		anim.PosY = -anim.CenterY + TILE_HALF_HEIGHT 
+	end
+
+	ANIMS[id] = anim
+end
 
 function modApi:createMechAnimations(animDefs)
 	Assert.Equals('table', type(animDefs), "Argument #1")
 
-	for file, animDef in pairs(animDefs) do
-		local filePath = ROOT_MECH..file..PNG
-		local fullFilePath = IMG..filePath
-
-		if not modApi:assetExists(fullFilePath) then
-			Assert.Error(string.format("Asset %q not found in resource.dat", fullFilePath))
+	for id, animDef in pairs(animDefs) do
+		if animDef.Image == nil then
+			animDef.Image = ROOT_MECH..id..PNG
 		end
 
-		local anim = ANIMS.MechUnit:new(animDef)
-		anim.Image = filePath
-
-		if anim.CenterX then
-			anim.PosX = -anim.CenterX
+		if animDef.Base == nil then
+			animDef.Base = "MechUnit"
 		end
 
-		if anim.CenterY then
-			anim.PosY = -anim.CenterY + TILE_HALF_HEIGHT 
+		createAnimation(id, animDef)
+	end
+end
+
+function modApi:createVekAnimations(animDefs)
+	Assert.Equals('table', type(animDefs), "Argument #1")
+
+	for id, animDef in pairs(animDefs) do
+		if animDef.Image == nil then
+			animDef.Image = ROOT_VEK..id..PNG
 		end
 
-		ANIMS[file] = anim
+		if animDef.Base == nil then
+			animDef.Base = "EnemyUnit"
+		end
+
+		createAnimation(id, animDef)
 	end
 end
 
 function modApi:createAnimations(animDefs)
 	Assert.Equals('table', type(animDefs), "Argument #1")
 
-	for file, animDef in pairs(animDefs) do
-		if type(animDef.Image) ~= 'string' then
-			Assert.Error("Animation 'Image' must be a 'string'")
-		end
-
-		local filePath = animDef.Image
-		local fullFilePath = IMG..filePath
-
-		if not modApi:assetExists(fullFilePath) then
-			Assert.Error(string.format("Asset %q not found in resource.dat", fullFilePath))
-		end
-
-		local base = animDef.Base or "Animation"
-		local baseAnim = ANIMS[base]
-
-		if baseAnim == nil then
-			Assert.Error(string.format("Base animation %s not found", base))
-		end
-
-		local anim = baseAnim:new(animDef)
-
-		if anim.CenterX then
-			anim.PosX = -anim.CenterX
-		end
-
-		if anim.CenterY then
-			anim.PosY = -anim.CenterY + TILE_HALF_HEIGHT 
-		end
-
-		ANIMS[file] = anim
+	for id, animDef in pairs(animDefs) do
+		createAnimation(id, animDef)
 	end
 end
