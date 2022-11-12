ceee1193160e905c868b76cdf6371fa606c64a3d
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-12 19:30:43 +0100

    Add CreateStructure
    Creates a structure and appends assets for it
    
    Example (from Into the Wild):
    ```lua
    CreateStructure{
    	Id = "Str_Depot",
    	Name = "Depot",
    	Path = "img/structures/",
    	Image = "depot",
    	ImageOffset = Point(-17,4),
    	Reward = REWARD_POWER
    }
    ```

--- scripts/mod_loader/modapi/global.lua
+++ scripts/mod_loader/modapi/global.lua
@@ -25,6 +25,55 @@ function CreatePilot(data)
 	end
 end
 
+function CreateStructure(structure)
+	Assert.ResourceDatIsOpen("Structures must be created on init")
+	Assert.Equals('table', type(structure), "Argument #1")
+
+	local id = structure.Id
+	local name = structure.Name
+	local path = structure.Path
+	local image = structure.Image
+	local imageOffset = structure.ImageOffset
+	local reward = structure.Reward
+
+	Assert.Equals('string', type(id), "Id")
+	Assert.Equals('nil', type(_G[id]), string.format("Structure with id %q already exists", structure.Id))
+	Assert.Equals('string', type(name), "Name")
+	Assert.Equals('string', type(path), "Path")
+	Assert.Equals('string', type(image), "Image")
+	Assert.TypePoint(imageOffset, "ImageOffset")
+	Assert.Equals('number', type(reward), "Reward")
+	Assert.Range(0, 2, reward, "Reward")
+
+	local resourcePath = modApi:getCurrentMod().resourcePath
+	local image_on = image.."_on.png"
+	local image_broken = image.."_broken.png"
+	local file_on = File(resourcePath, path..image_on)
+	local file_broken = File(resourcePath, path..image_broken)
+	local assetsRoot = "img/combat/structures/"
+	local assetpath_on = assetsRoot..image_on
+	local assetpath_broken= assetsRoot..image_broken
+	local textId = id.."_Name"
+
+	Assert.True(file_on:exists())
+	Assert.True(file_broken:exists())
+	Assert.False(modApi:assetExists(assetpath_on), "Asset '"..assetpath_on.."' already exists")
+	Assert.False(modApi:assetExists(assetpath_broken), "Asset '"..assetpath_broken.."' already exists")
+	Assert.Equals('nil', type(Mission_Texts[textId]), "Mission Text '"..textId.."' already exists")
+
+	modApi:appendAsset(assetpath_on, file_on:relative_path())
+	modApi:appendAsset(assetpath_broken, file_broken:relative_path())
+	Location[assetpath_on:sub(5,-1)] = imageOffset
+	Location[assetpath_broken:sub(5,-1)] = imageOffset
+	Mission_Texts[textId] = name
+
+	_G[structure.Id] = {
+		Name = name,
+		Image = image,
+		Reward = reward
+	}
+end
+
 function IsTestMechScenario()
 	if not Game then return false end
 
