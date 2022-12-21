73d0311117c2bab5d364da09d10385ab83a7ee5d
tos-x <61102907+tos-x@users.noreply.github.com>
2022-12-21 14:37:44 -0500

    Update default_maps.lua

--- scripts/mod_loader/modapi/default_maps.lua
+++ scripts/mod_loader/modapi/default_maps.lua
@@ -4,6 +4,8 @@ return {
 	"acid10", "acid11", "acid12", "acid13", "acid14",
 	"acid15",
 
+	"ae_trailer",
+
 	"any0", "any1", "any2", "any3", "any4",
 	"any5", "any6", "any7", "any8", "any9",
 	"any10", "any11", "any12", "any13", "any14",
@@ -15,15 +17,29 @@ return {
 	"any40", "any41", "any42", "any43", "any44",
 	"any45", "any46", "any47", "any48", "any49",
 	"any50",
+	
+	"anyAE1", "anyAE2", "anyAE3", "anyAE4", "anyAE5",
+	"anyAE6", "anyAE7", "anyAE8", "anyAE9", "anyAE10",
+	"anyAE11", "anyAE12", "anyAE13", "anyAE14", "anyAE15",
+	"anyAE16", "anyAE17", "anyAE18", "anyAE19", "anyAE20",
+	"anyAE21", "anyAE22", "anyAE23", "anyAE24", "anyAE25",
+	"anyAE26", "anyAE27", "anyAE28", "anyAE29", "anyAE30",
+	"anyAE31", "anyAE32", "anyAE33", "anyAE34", "anyAE35",
+	"anyAE36", "anyAE37", "anyAE38", "anyAE39", "anyAE40",
 
 	"barrels", "belt",
 
 	"cave1", "cave2", "cave3", "cave4", "cave5",
+	
+	"caveAE1", "caveAE2", "caveAE3", "caveAE4",
 
 	"crosscrack", "crosscrack2", "crosscrack3", "crosscrack4",
 	"crosscrack5", "crosscrack6", "crosscrack7", "crosscrack8",
 	"crosscrack9", "crosscrack10", "crosscrack11", "crosscrack12",
 	"crosscrack13", "crosscrack14", "crosscrack15",
+	
+	"crosscrackAE1", "crosscrackAE2", "crosscrackAE3", "crosscrackAE4",
+	"crosscrackAE5",
 
 	"ctf0", "ctf1", "ctf2", "ctf3", "ctf4",
 	"ctf5", "ctf6", "ctf7", "ctf8", "ctf9",
@@ -34,6 +50,8 @@ return {
 	"disposal10", "disposal11", "disposal12", "disposal13", "disposal14",
 	"disposal15", "disposal16", "disposal17", "disposal18", "disposal19",
 	"disposal20",
+	
+	"disposalAE1", "disposalAE2", "disposalAE3", "disposalAE4", "disposalAE5",
 
 	"final_island", "final_mission",
 
@@ -48,6 +66,9 @@ return {
 	"hightide11", "hightide12", "hightide13", "hightide14", "hightide15",
 	"hightide16", "hightide17", "hightide18", "hightide19", "hightide20",
 
+	"hightideAE1", "hightideAE2", "hightideAE3", "hightideAE4", "hightideAE5",
+	"hightideAE6", "hightideAE7",
+	
 	"island_ending",
 
 	"matt",
@@ -62,6 +83,8 @@ return {
 	"opendam6", "opendam7", "opendam8", "opendam9", "opendam10",
 	"opendam11", "opendam12", "opendam13", "opendam14", "opendam15",
 	"opendam16", "opendam17", "opendam18", "opendam19", "opendam20",
+	
+	"opendamAE1", "opendamAE2", "opendamAE3", "opendamAE4",
 
 	"sand0", "sand1", "sand2", "sand3", "sand4",
 	"sand5", "sand6", "sand7", "sand8", "sand9",
@@ -69,6 +92,8 @@ return {
 	"sand15",
 
 	"sand_hole1", "sand_hole2", "sand_hole3", "sand_hole4",
+	"sand_holeAE1", "sand_holeAE2", "sand_holeAE3", "sand_holeAE4",
+	"sand_holeAE5", "sand_holeAE6",
 
 	"snow0", "snow1", "snow2", "snow3", "snow4",
 	"snow5", "snow6", "snow7", "snow8", "snow9",
@@ -80,6 +105,9 @@ return {
 	"terraformer1", "terraformer2", "terraformer3", "terraformer4",
 	"terraformer5", "terraformer6", "terraformer7", "terraformer8",
 
+	"terraformerAE1", "terraformerAE2", "terraformerAE3", "terraformerAE4",
+	"terraformerAE5",
+	
 	"trailer",
 
 	"train0", "train1", "train2", "train3", "train4",
@@ -88,9 +116,16 @@ return {
 	"train15", "train16", "train17", "train18", "train19",
 	"train20",
 
+	"trainAE1", "trainAE2", "trainAE3", "trainAE4", "trainAE5",
+	"trainAE6", "trainAE7", "trainAE8", "trainAE9", "trainAE10",
+	
 	"tutorial",
 
 	"volcano1", "volcano2", "volcano3", "volcano4",
 	"volcano5", "volcano6", "volcano7", "volcano8",
-	"volcano9", "volcano10"
+	"volcano9", "volcano10",
+	
+	"volcanoAE1", "volcanoAE2", "volcanoAE3", "volcanoAE4",
+	"volcanoAE5"
+	
 }
