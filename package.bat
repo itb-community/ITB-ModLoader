f989fc7aad537ccb8f87811023d6bc0117d58aed
KnightMiner <knightminer4@gmail.com>
2023-03-06 19:43:06 -0500

    Synchronize release scripts by using shared code
    We don't need to have two separate release scripts, espeically when each of them did different things.
    Added _release.bat which is the shared code for building a release directory in both scripts so we are not maintaining this in two places.

--- package.bat
+++ package.bat
@@ -1,20 +1,6 @@
 @echo off
 
-IF EXIST release RMDIR /q /s release
-MKDIR release
-
-REM Copy required files into release directory...
-COPY /V lua5.1.dll release\lua5.1.dll
-COPY /V lua5.1-original.dll release\lua5.1-original.dll
-COPY /V opengl32.dll release\opengl32.dll
-COPY /V SDL2.dll release\SDL2.dll
-COPY /V SDL2-original.dll release\SDL2-original.dll
-COPY /v ftldat.dll release\ftldat.dll
-COPY /V README.md release\MODLOADER_README.txt
-COPY /V uninstall_modloader.bat release\uninstall_modloader.bat
-XCOPY mods release\mods /s /e /i
-XCOPY scripts release\scripts /s /e /i
-XCOPY resources release\resources /s /e /i
+CALL _release.bat
 
 REM ...and pack it into a zip
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('release', 'ITB-ModLoader-#.#.#.zip'); }"
