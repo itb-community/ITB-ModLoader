0e564db6826b34388930f0434b65c853310423a2
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-07-15 08:42:49 -0400

    Adding vek spawning end and pre/post env plan hooks

--- scripts/mod_loader/altered/missions.lua
+++ scripts/mod_loader/altered/missions.lua
@@ -1,4 +1,3 @@
-
 function Mission:OnSerializationStart(t)
 	t.MissionEndImpl = self.MissionEndImpl
 	t.MissionEnd = self.MissionEnd
@@ -117,13 +116,13 @@ end
 function Mission:MissionEndImpl()
 	local ret = SkillEffect()
 	local enemy_count = Board:GetEnemyCount()
-	
+
 	if enemy_count == 0 then
 		ret:AddVoice("MissionEnd_Dead", -1)
 	elseif self.RetreatEndingMessage then
 		ret:AddVoice("MissionEnd_Retreat", -1)
 	end
-	
+
 	if self:GetDamage() == 0 then
 		ret:AddScript("Board:StartPopEvent(\"Closing_Perfect\")")
 	elseif self:GetDamage() > 4 then
@@ -159,7 +158,7 @@ function Mission:MissionEndImpl()
 	effect.fDelay = 0.5
 
 	modApi:firePreprocessVekRetreatHooks(self, ret)
-	
+
 	local retreated = 0
 	for _, p in ipairs(Board) do
 		if Board:IsPawnTeam(p,TEAM_ENEMY) then
@@ -178,9 +177,9 @@ function Mission:MissionEndImpl()
 	end
 
 	modApi:firePostprocessVekRetreatHooks(self, ret)
-	
+
 	ret:AddDelay(math.max(0,4 - retreated * 0.5))
-		
+
 	Board:AddEffect(ret)
 end
 
@@ -192,7 +191,7 @@ end
 
 function Mission:MissionEnd()
 	self:MissionEndImpl()
-	
+
 	local fx = SkillEffect()
 	modApi:fireMissionEndHooks(self, fx)
 	fx:AddScript("modApi.runLaterQueue = {}")
@@ -221,11 +220,11 @@ function Mission:BaseStart(suppressHooks)
 	-- we want to insert a new SetupDifficulty() function before
 	-- environment start.
 	self.VoiceEvents = {}
-	
+
 	if self.AssetId ~= "" then
 		self.AssetLoc = Board:AddUniqueBuilding(_G[self.AssetId].Image)
 	end
-	
+
 	self.LiveEnvironment = _G[self.Environment]:new()
 	-- TODO: is this still needed? without it we could directly call the old logic
 	-- what makes this different from SetupDiffMod?
@@ -233,7 +232,7 @@ function Mission:BaseStart(suppressHooks)
 
 	self.LiveEnvironment:Start()
 	self:StartMission()
-	
+
 	self:SetupDiffMod()
 
 	for index, obj in ipairs(self.BonusObjs) do
@@ -323,7 +322,7 @@ end
 -- The vanilla function checks this with LiveEnvironment:IsEffect()
 -- The mod loader wants ApplyEnvironmentEffect to be called at least once
 -- to dispatch environment effect events, so we add additional logic.
--- 
+--
 -- We will use an additional 'processed' flag to handle this logic in
 -- Mission:IsEnvironmentEffect and Mission:ApplyEnvironmentEffect
 Mission.IsEnvironmentEffectVanilla = Mission.IsEnvironmentEffect
@@ -335,6 +334,31 @@ Mission.IsEnvironmentEffect = function(self)
 	end
 
 	return not self.LiveEnvironment.processed
+end
+
+Mission.PlanEnvironmentVanilla = Mission.PlanEnvironment
+Mission.PlanEnvironment = function(self, ...)
+	local turn = Game:GetTurnCount()
+
+	-- The first time this is called each turn, fire the
+	-- pre plan hooks and return true which causes the game
+	-- to re-call this function after allowing a break for
+	-- events and such to run
+	if self.envPlanHooksTurn ~= turn then
+		self.envPlanHooksTurn = turn
+		modApi:fireVekSpawningEndHooks(self)
+		modApi:firePreEnvPlannedHooks(self)
+		return true
+	end
+
+	local result = self:PlanEnvironmentVanilla(...)
+
+	-- If false is returned the game will not call this again
+	-- so the environment planning is complete
+	if not result then
+		modApi:firePostEnvPlannedHooks(self)
+	end
+	return result
 end
 
 
@@ -369,13 +393,13 @@ function Mission:GetSaveData()
 		repeat
 			local regionData = RegionData["region"..i]
 			i = i + 1
-	
+
 			if regionData and GAME:GetMissionId(regionData.mission) == mission_id then
 				return regionData.player
 			end
 		until regionData == nil
 	end
-	
+
 	return nil
 end
 
